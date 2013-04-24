4cfd83c0035dc8432fc514c38798f6e1a52224a2
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-04-24 19:53:08 +0200

    Développement de base du plugin export_embed et traductions

--- mod/export_embed/external_embed.php
+++ mod/export_embed/external_embed.php
@@ -20,32 +20,102 @@ require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
   - en l'absence de mot de passe ou de clef (ou si vide) => vide ou invite de connexion ou invite à saisir le mot de passe selon les cas
 */
 
-$embed = get_input('embed', false);
-$guid = get_input('guid', false);
-$limit = get_input('limit', 5); if ($limit > 100) $limit = 100;
-$body = '';
+global $CONFIG;
 
+$embedurl = get_input('embedurl', false);
+if (empty($embedurl)) {
+  $site_url = get_input('site_url', false);
+  $embedtype = get_input('embedtype', false);
+}
+$group_guid = get_input('group_guid', false);
+$user_guid = get_input('user_guid', false);
+$limit = get_input('limit', 5); if ($limit > 100) $limit = 100;
+$offset = get_input('offset', 0);
+$params = get_input('params', 0);
+$key = get_input('key', false);
+
+$body = '';
 $style = '';
 
-// echo "Paramètres $embed $guid $limit";
+// echo "Paramètres $embedtype $group_guid $limit";
 
-switch ($embed) {
+switch ($embedtype) {
+  
   // fil d'activité global
   case 'site_activity':
     $title = "Activité de " . $CONFIG->site->name;
-    $body = elgg_list_river(array('limit' => 5, 'pagination' => false));
-    if (!$content) { $body = elgg_echo('river:none'); }
-    $body .= '<span class="elgg-widget-more"><a href="' . $vars['url'] . 'activity">Voir toute l\'activité du site</a></span>';
+    $options = array('limit' => $limit, 'offset' => $offset, 'pagination' => true);
+    $body = elgg_list_river($options);
+    if (!$body) { $body = elgg_echo('river:none'); }
+    $body .= '<span class="elgg-widget-more"><a href="' . $CONFIG->url . 'activity">Voir toute l\'activité du site</a></span>';
     break;
   
-  // liste des groupes publics (icône + titre) - aléatoire parmi les groupes en Une
+  // fil d'activité de ses contacts
+  case 'friends_activity':
+    $title = "Activité de mes contacts";
+    $options = array('limit' => $limit, 'offset' => $offset, 'pagination' => true);
+		$options['relationship_guid'] = elgg_get_logged_in_user_guid();
+		$options['relationship'] = 'friend';
+    $body = elgg_list_river($options);
+    if (!$body) { $body = elgg_echo('river:none'); }
+    $body .= '<span class="elgg-widget-more"><a href="' . $CONFIG->url . 'activity">Voir toute l\'activité du site</a></span>';
+    break;
+  
+  // fil d'activité personnel
+  case 'my_activity':
+    $title = "Activité de " . elgg_get_logged_in_user_entity()->name;
+    $options = array('limit' => $limit, 'offset' => $offset, 'pagination' => true);
+    $options['subject_guid'] = elgg_get_logged_in_user_guid();
+    $body = elgg_list_river($options);
+    if (!$body) { $body = elgg_echo('river:none'); }
+    $body .= '<span class="elgg-widget-more"><a href="' . $CONFIG->url . 'activity">Voir toute l\'activité du site</a></span>';
+    break;
+  
+  // Activité d'un groupe
+  case 'group_activity':
+    $ignore_access = elgg_get_ignore_access();
+    elgg_set_ignore_access(true);
+    $group = get_entity($group_guid);
+    elgg_set_ignore_access($ignore_access);
+    // Test group activity
+    if ($group_guid && elgg_instanceof($group, 'group')) {
+      $title = "Activité récente du groupe " . $group->name . " de " . $CONFIG->site->name;
+      $body .= "<div class=\"embed embed-group-activity\"><h4>" . '<a href="' . $group->getURL() . '">' . $group->name . "</a></h4></div>";
+      $db_prefix = elgg_get_config('dbprefix');
+      $activity = '';
+      $activities_count = elgg_get_river(array(
+	      'count' => true,
+	      'joins' => array("JOIN {$db_prefix}entities e1 ON e1.guid = rv.object_guid"),
+	      'wheres' => array("(e1.container_guid = $group_guid)"),
+      ));
+      //$activity .= "Activités : $activities_count<br />";
+      $activities = elgg_get_river(array(
+	      'limit' => $activities_count,
+	      'joins' => array("JOIN {$db_prefix}entities e1 ON e1.guid = rv.object_guid"),
+	      'wheres' => array("(e1.container_guid = $group_guid)"),
+      ));
+error_log('EXT EMBED');
+      foreach ($activities as $item) {
+		    if (elgg_instanceof($item)) {
+			    $id = "elgg-{$item->getType()}-{$item->getGUID()}";
+		    } else {
+			    $id = "item-{$item->getType()}-{$item->id}";
+		    }
+		    $activity .= '<div id="'.$id.'">' . elgg_view_list_item($item, $vars) . '</div>';
+      }
+      if (empty($activity)) { $activity = '<p>' . elgg_echo('dashboard:widget:group:noactivity') . '</p>'; }
+      $body .= '<div class="embed-group-content">' . $activity . '</div>';
+    } else { $body = '<p>Pas d\'accès au groupe ou GUID du groupe incorrect</p>'; }
+    break;
+  
+  // liste des groupes publics (icône + titre)
   case 'groups_list':
     $title = "Liste des groupes de " . $CONFIG->site->name;
     $groups = elgg_get_entities(array('types' => 'group', 'limit' => 9999, 'reverse_order_by' => false));
     $body = elgg_view_entity_list($groups, '', 0, 99, false, false, false);
     break;
   
-  // bloc agenda
+  // agenda du site
   case 'agenda':
     $title = "Agenda de " . $CONFIG->site->name;
     // Load event calendar model
@@ -62,77 +132,49 @@ switch ($embed) {
     }
     // If there are any events to view, view them
     if (is_array($events) && sizeof($events) > 0) {
-      $body .= "<div id=\"widget_calendar\">";
+      $body .= '<div id="widget_calendar">';
       foreach($events as $event) {
-        $body .= elgg_view("object/event_calendar",array('entity' => $event));
+        $body .= elgg_view("object/event_calendar", array('entity' => $event));
       }
-      $body .= "</div>";
+      $body .= '</div>';
     }
     break;
   
-  // Activité d'un groupe
-  case 'group_activity':
-    $ignore_access = elgg_get_ignore_access();
-    elgg_set_ignore_access(true);
-    $group = get_entity($guid);
-    elgg_set_ignore_access($ignore_access);
-    
-    // Test group activity
-    if ($guid && elgg_instanceof($group, 'group')) {
-      $title = "Activité récente du groupe " . $group->name . " de " . $CONFIG->site->name;
-      $body .= "<div class=\"embed embed-group-activity\"><h4>" . '<a href="' . $group->getURL() . '">' . $group->name . "</a></h4></div>";
-      $db_prefix = elgg_get_config('dbprefix');
-      $activity = '';
-      $activities_count = elgg_get_river(array(
-	      'count' => true,
-	      'joins' => array("JOIN {$db_prefix}entities e1 ON e1.guid = rv.object_guid"),
-	      'wheres' => array("(e1.container_guid = $guid)"),
-      ));
-      //$activity .= "Activités : $activities_count<br />";
-      $activities = elgg_get_river(array(
-	      'limit' => $activities_count,
-	      'joins' => array("JOIN {$db_prefix}entities e1 ON e1.guid = rv.object_guid"),
-	      'wheres' => array("(e1.container_guid = $guid)"),
-      ));
-error_log('EXT EMBED');
-      foreach ($activities as $item) {
-        /*
-        [id] => 2
-        [subject_guid] => 41
-        [object_guid] => 73
-        [annotation_id] => 0
-        [type] => object
-        [subtype] => blog
-        [action_type] => create
-        [access_id] => 2
-        [view] => river/object/blog/create
-        [posted] => 1351020437
-        $activity .= $item->id . ', ' . $item->subject_guid . ', ' . $item->object_guid . ', ' . $item->annotation_id . ', ' . $item->type . ', ' . $item->subtype . ', ' . $item->action_type . ', ' . $item->access_id . ', ' . $item->view . ', ' . $item->posted . ', ' . '<hr />';
-        */
-		    if (elgg_instanceof($item)) {
-			    $id = "elgg-{$item->getType()}-{$item->getGUID()}";
-		    } else {
-			    $id = "item-{$item->getType()}-{$item->id}";
-		    }
-		    $activity .= '<div id="'.$id.'">' . elgg_view_list_item($item, $vars) . '</div>';
-      }
-      if (empty($activity)) { $activity = '<p>' . elgg_echo('dashboard:widget:group:noactivity') . '</p>'; }
-      $body .= '<div class="embed-group-content">' . $activity . '</div>';
-    } else { $body = '<p>Pas d\'accès au groupe ou GUID du groupe incorrect</p>'; }
-    break;
-    
+  
+  // Defaults to help
   default:
-    // Don't accept unhandled values
-    $body .= "Pas de paramètre ou les paramètres fournis sont invalides.<br />Paramètres valides : embed, guid, limit";
-    // @TODO : do better : explain the embed API...
+    $body .= "<p>Ce widget n'est pas encore configuré (ou les paramètres fournis sont invalides).</p>
+      <p>
+        Pour afficher les widgets de ce site sur un autre site, veuillez utiliser les informations suivantes&nbsp;:
+        <ul>
+          <li>Adresse du site&nbsp;: <strong>" . $CONFIG->url . "</strong></li>
+          <li>Types de widgets&nbsp;:
+            <ul>
+            </ul>
+          </li>
+          >> <strong>" . $CONFIG->url . "embed/site_activity</strong> pour l'activité générale du site</li>
+          <li><strong>friends_activity</strong> pour l'activité de vos contacts</li>
+          <li><strong>my_activity</strong> pour votre activité</li>
+          <li><strong>group_activity&group_guid=<em>XXXXX</em></strong> pour l'activité d'un groupe en particulier : remplacer <strong><em>XXXXX</em></strong> par le numéro du groupe à afficher, que vous trouverez dans l'adresse de la page d'accueil du groupe : <em>groups/profile/<strong>XXXXX</strong>/nom-du-groupe</em></li>
+          <li><strong>groups_list</strong> pour la liste des groupes du site</li>
+          <li><strong>agenda</strong> pour l'agenda du site</li>
+        </ul>
+      </p>";
 }
+
+
 // Message si vide
 if (empty($body)) {
   $body = '<p>Pas de contenu pour le moment.</p>';
 }
+
 // Alerte si non connecté
-if (!elgg_is_logged_in()) $body = "<div style=\"background:#FAA; border:1px dashed #A00; padding:3px 6px; margin:3px;\">ATTENTION, vous n'êtes pas connecté sur " . $CONFIG->site->name . ". Veuillez <a href=\"" . $CONFIG->url . "\" target=\"_top\">vous connecter</a> pour accéder au contenu réservé aux membres.</div>" . $body;
-else $body .= '<div style="background:#AFA; border:1px dashed #0A0; padding:3px 6px; margin:3px;"><a href="' . $CONFIG->url . '" target="_top">Accéder au réseau dans une nouvelle fenêtre</a>.</div>' . $body;
+if (!elgg_is_logged_in()) {
+  $body = "<div style=\"background:#FAA; border:1px dashed #A00; padding:3px 6px; margin:3px;\">ATTENTION, vous n'êtes pas connecté sur " . $CONFIG->site->name . ". Veuillez <a href=\"" . $CONFIG->url . "\" target=\"_top\">vous connecter</a> pour accéder au contenu réservé aux membres.</div>" . $body;
+} else {
+  $body = '<div style="background:#AFA; border:1px dashed #0A0; padding:3px 6px; margin:3px;"><a href="' . $CONFIG->url . '" target="_top">Accéder à ' . $CONFIG->sitename . ' dans une nouvelle fenêtre</a>.</div>' . $body;
+}
+
 
 
 // Display page
--- mod/export_embed/views/default/widgets/export_embed/content.php
+++ mod/export_embed/views/default/widgets/export_embed/content.php
@@ -3,11 +3,15 @@
  * User blog widget display view
  */
 
-$widget_url = $vars['entity']->url;
+$embedurl = $vars['entity']->embedurl;
 
-if (!empty($widget_url)) {
-  echo '<iframe src="' . html_entity_decode($widget_url) . '" style="height:600px; overflow-y:auto; width:288px;">Chargement en cours</iframe>';
-} else {
-  echo "Veuillez configurer le widget !";
+if (empty($embedurl)) {
+  $site_url = $vars['entity']->site_url;
+  $embedtype = $vars['entity']->embedtype;
+  $embedparams = $vars['entity']->params;
 }
+
+if (empty($embedurl)) $embedurl = $vars['url'] . 'embed'; // En local => Aide du widget
+
+echo '<iframe src="' . html_entity_decode($embedurl) . '" style="height:600px; overflow-y:auto; width:288px;">Chargement en cours</iframe>';
 
