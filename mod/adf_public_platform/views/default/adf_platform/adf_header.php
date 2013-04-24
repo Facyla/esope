94dd407a8f3cc03a177cb059883a87ad5866e196
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-04-24 11:34:20 +0200

    Plugins requis vs suggérés et encore des vérification de plugins activés pour le menu

--- mod/adf_public_platform/views/default/adf_platform/adf_header.php
+++ mod/adf_public_platform/views/default/adf_platform/adf_header.php
@@ -13,11 +13,29 @@ if (elgg_is_logged_in()) {
   
   // Liste de ses groupes
   $groups = '';
-  $mygroups = elgg_get_entities_from_relationship(array( 'type' => 'group', 'relationship' => 'member', 'relationship_guid' => $ownguid, 'inverse_relationship' => false, 'limit' => 99, 'order_by' => 'time_created asc'));
-  foreach ($mygroups as $group) {
-    $groups .= '<li><a href="' . $group->getURL() . '">' 
-      . '<img src="' . $group->getIconURL('tiny') . '" alt="' . str_replace('"', "''", $group->name) . ' (' . elgg_echo('adf_platform:groupicon') . '" />'
-      . $group->name . '</a></li>';
+  if (elgg_is_active_plugin('groups')) {
+    $mygroups = elgg_get_entities_from_relationship(array( 'type' => 'group', 'relationship' => 'member', 'relationship_guid' => $ownguid, 'inverse_relationship' => false, 'limit' => 99, 'order_by' => 'time_created asc'));
+    foreach ($mygroups as $group) {
+      $groups .= '<li><a href="' . $group->getURL() . '">' 
+        . '<img src="' . $group->getIconURL('tiny') . '" alt="' . str_replace('"', "''", $group->name) . ' (' . elgg_echo('adf_platform:groupicon') . '" />'
+        . $group->name . '</a></li>';
+    }
+  // "Invitations" dans les groupes : affiché seulement s'il y a des invitations en attente
+    $group_invites = groups_get_invited_groups(elgg_get_logged_in_user_guid());
+    $invites_count = sizeof($group_invites);
+    if ($invites_count == 1) {
+      $invites = '<li class="group-invites"><a href="' . $url . 'groups/invitations/' . $ownusername . '" title="' . $invites_count . ' ' . elgg_echo('adf_platform:groupinvite') . '">' . $invites_count . '</a></li>';
+    } else if ($invites_count > 1) {
+      $invites = '<li class="group-invites"><a href="' . $url . 'groups/invitations/' . $ownusername . '" title="' . $invites_count . ' ' . elgg_echo('adf_platform:groupinvites') . '">' . $invites_count . '</a></li>';
+    }
+    // Demandes de contact en attente : affiché seulement s'il y a des demandes en attente
+	  $friendrequests_options = array("type" => "user", "count" => true, "relationship" => "friendrequest", "relationship_guid" => $own->guid, "inverse_relationship" => true);
+    $friendrequests_count = elgg_get_entities_from_relationship($friendrequests_options);
+    if ($friendrequests_count == 1) {
+      $friendrequests = '<li class="invites"><a href="' . $url . 'friend_request/' . $ownusername . '" title="' . $friendrequests_count . ' ' . elgg_echo('adf_platform:friendinvite') . '">' . $friendrequests_count . '</a></li>';
+    } else if ($friendrequests_count > 1) {
+      $friendrequests = '<li class="invites"><a href="' . $url . 'friend_request/' . $ownusername . '" title="' . $friendrequests_count . ' ' . elgg_echo('adf_platform:friendinvites') . '">' . $friendrequests_count . '</a></li>';
+    }
   }
   
   // Liste des catégories (thématiques du site)
@@ -30,28 +48,13 @@ if (elgg_is_logged_in()) {
   }
   
   // Messages non lus
-  $num_messages = (int)messages_count_unread();
-  if ($num_messages != 0) {
-    $text = "$num_messages";
-    $tooltip = elgg_echo("messages:unreadcount", array($num_messages));
-    $messages = '<li class="invites"><a href="' . $url . 'messages/inbox/' . $ownusername . '" title="' .  $tooltip . '">' . $text . '</a></li>';
-  }
-  
-  // "Invitations" dans les groupes : affiché seulement s'il y a des invitations en attente
-  $group_invites = groups_get_invited_groups(elgg_get_logged_in_user_guid());
-  $invites_count = sizeof($group_invites);
-  if ($invites_count == 1) {
-    $invites = '<li class="group-invites"><a href="' . $url . 'groups/invitations/' . $ownusername . '" title="' . $invites_count . ' ' . elgg_echo('adf_platform:groupinvite') . '">' . $invites_count . '</a></li>';
-  } else if ($invites_count > 1) {
-    $invites = '<li class="group-invites"><a href="' . $url . 'groups/invitations/' . $ownusername . '" title="' . $invites_count . ' ' . elgg_echo('adf_platform:groupinvites') . '">' . $invites_count . '</a></li>';
-  }
-  // Demandes de contact en attente : affiché seulement s'il y a des demandes en attente
-	$friendrequests_options = array("type" => "user", "count" => true, "relationship" => "friendrequest", "relationship_guid" => $own->guid, "inverse_relationship" => true);
-  $friendrequests_count = elgg_get_entities_from_relationship($friendrequests_options);
-  if ($friendrequests_count == 1) {
-    $friendrequests = '<li class="invites"><a href="' . $url . 'friend_request/' . $ownusername . '" title="' . $friendrequests_count . ' ' . elgg_echo('adf_platform:friendinvite') . '">' . $friendrequests_count . '</a></li>';
-  } else if ($friendrequests_count > 1) {
-    $friendrequests = '<li class="invites"><a href="' . $url . 'friend_request/' . $ownusername . '" title="' . $friendrequests_count . ' ' . elgg_echo('adf_platform:friendinvites') . '">' . $friendrequests_count . '</a></li>';
+  if (elgg_is_active_plugin('messages')) {
+    $num_messages = (int)messages_count_unread();
+    if ($num_messages != 0) {
+      $text = "$num_messages";
+      $tooltip = elgg_echo("messages:unreadcount", array($num_messages));
+      $messages = '<li class="invites"><a href="' . $url . 'messages/inbox/' . $ownusername . '" title="' .  $tooltip . '">' . $text . '</a></li>';
+    }
   }
   
 }
@@ -104,50 +107,53 @@ if (elgg_is_logged_in()) {
             <nav>
               <ul>
                 <li class="home"><a href="<?php echo $url; ?>" <?php if (full_url() == $url) { echo 'class="active elgg-state-selected"'; } ?> >Accueil</a>
-                  <ul>
-                    <li><a href="<?php echo $url; ?>" ><?php echo elgg_echo('dashboard'); ?></a></li>
-                    <li><a href="<?php echo $url; ?>activity" ><?php echo elgg_echo('activity'); ?></a></li>
-                  </ul>
+                  <?php if (elgg_is_active_plugin('dashboard')) { ?>
+                    <ul>
+                      <li><a href="<?php echo $url; ?>" ><?php echo elgg_echo('dashboard'); ?></a></li>
+                      <li><a href="<?php echo $url; ?>activity" ><?php echo elgg_echo('activity'); ?></a></li>
+                    </ul>
+                  <?php } ?>
                 </li>
                 
                 <?php /* activity : Fil d'activité du site */ ?>
                 
                 <?php if (elgg_is_active_plugin('groups')) { ?>
-                <li class="groups"><a <?php if(elgg_in_context('groups') || (elgg_instanceof(elgg_get_page_owner_entity(), 'group'))) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url; ?>groups/all"><?php echo elgg_echo('groups'); ?></a>
-                  <ul>
-                    <li><a href="<?php echo $url . 'groups/all'; ?>"><?php echo elgg_echo('adf_platform:joinagroup'); ?></a></li>
-                    <?php echo $groups; ?>
-                  </ul>
-                </li>
-                <?php echo $invites; ?>
+                  <li class="groups"><a <?php if(elgg_in_context('groups') || (elgg_instanceof(elgg_get_page_owner_entity(), 'group'))) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url; ?>groups/all"><?php echo elgg_echo('groups'); ?></a>
+                    <ul>
+                      <li><a href="<?php echo $url . 'groups/all'; ?>"><?php echo elgg_echo('adf_platform:joinagroup'); ?></a></li>
+                      <?php echo $groups; ?>
+                    </ul>
+                  </li>
+                  <?php echo $invites; ?>
                 <?php } ?>
                 
                 <?php if (elgg_is_active_plugin('categories')) { ?>
-                <li class="thematiques"><a <?php if(elgg_in_context('categories')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'categories'; ?>"><?php echo elgg_echo('adf_platform:categories'); ?></a>
-                  <ul>
-                    <li><a href="<?php echo $url; ?>categories"><?php echo elgg_echo('adf_platform:categories:all'); ?></a></li>
-                    <?php echo $categories; ?>
-                  </ul>
-                </li>
+                  <li class="thematiques"><a <?php if(elgg_in_context('categories')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'categories'; ?>"><?php echo elgg_echo('adf_platform:categories'); ?></a>
+                    <ul>
+                      <li><a href="<?php echo $url; ?>categories"><?php echo elgg_echo('adf_platform:categories:all'); ?></a></li>
+                      <?php echo $categories; ?>
+                    </ul>
+                  </li>
                 <?php } ?>
                 
                 <?php if (elgg_is_active_plugin('members')) { ?>
-                <li class="members"><a <?php if(elgg_in_context('members') || elgg_in_context('profile')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'members'; ?>"><?php echo elgg_echo('adf_platform:directory'); ?></a></li>
+                  <li class="members"><a <?php if(elgg_in_context('members') || elgg_in_context('profile')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'members'; ?>"><?php echo elgg_echo('adf_platform:directory'); ?></a></li>
                 <?php } ?>
                 
                 <?php if (elgg_is_active_plugin('event_calendar')) { ?>
-                <li class="agenda"><a <?php if (elgg_in_context('event_calendar') && !elgg_in_context('groups')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'event_calendar/list'; ?>"><?php echo elgg_echo('adf_platform:event_calendar'); ?></a></li>
+                  <li class="agenda"><a <?php if (elgg_in_context('event_calendar') && !elgg_in_context('groups')) { echo 'class="active elgg-state-selected"'; } ?> href="<?php echo $url . 'event_calendar/list'; ?>"><?php echo elgg_echo('adf_platform:event_calendar'); ?></a></li>
                 <?php } ?>
                 
               </ul>
             </nav>
+            
             <?php if (elgg_is_active_plugin('search')) { ?>
-            <form action="<?php echo $url . 'search'; ?>" method="post">
-              <?php $search_text = 'Trouvez des groupes, des fichiers...'; ?>
-              <label for="adf-search-input" class="invisible"><?php echo $search_text; ?></label>
-              <input type="text" id="adf-search-input" name="q" value="<?php echo $search_text; ?>" />
-              <input type="image" id="adf-search-submit-button" src="<?php echo $urlicon; ?>recherche.png" value="<?php echo elgg_echo('adf_platform:search'); ?>" />
-            </form>
+              <form action="<?php echo $url . 'search'; ?>" method="post">
+                <?php $search_text = 'Trouvez des groupes, des fichiers...'; ?>
+                <label for="adf-search-input" class="invisible"><?php echo $search_text; ?></label>
+                <input type="text" id="adf-search-input" name="q" value="<?php echo $search_text; ?>" />
+                <input type="image" id="adf-search-submit-button" src="<?php echo $urlicon; ?>recherche.png" value="<?php echo elgg_echo('adf_platform:search'); ?>" />
+              </form>
             <?php } ?>
           </div>
         </div>
