16bf8ca31e9dd43e7440716a4451fa8dd2cdda3a
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-04-30 14:24:32 +0200

    debug...

--- mod/prevent_notifications/start.php
+++ mod/prevent_notifications/start.php
@@ -13,11 +13,12 @@ elgg_register_event_handler('init', 'system', 'prevent_notifications_init'); // 
 function prevent_notifications_init() {
   global $CONFIG;
   // Hook pour bloquer les notifications si on a demandé à les désactiver
-  register_plugin_hook('object:notifications', 'all', 'prevent_notifications_object_notifications_disable', 0);
+  elgg_register_plugin_hook_handler('object:notifications', 'all', 'prevent_notifications_object_notifications_disable', 0);
 }
 
 function prevent_notifications_object_notifications_disable($hook, $entity_type, $returnvalue, $params) {
   $send_notification = get_input('send_notification', 'yes');
+register_error("PREVENT NOTIF : block ? => $send_notification");
 error_log("PREVENT NOTIF : block ? => $send_notification");
   if ($send_notification == 'no') {
 error_log("PREVENT NOTIF : bloquée...");
