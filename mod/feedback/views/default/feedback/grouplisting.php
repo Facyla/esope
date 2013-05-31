dae876040f0b688f4c1301d282f42826d121a259
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-05-31 10:59:14 +0200

    Feedback : link to view all in groups + pagination

--- mod/feedback/views/default/feedback/grouplisting.php
+++ mod/feedback/views/default/feedback/grouplisting.php
@@ -2,20 +2,22 @@
 // Give access to feedbacks in groups
 $feedbackgroup = elgg_get_plugin_setting('feedbackgroup', "feedback");
 if (!empty($feedbackgroup) && ($feedbackgroup != 'no') && elgg_is_logged_in()) {
-  $page_owner = elgg_get_page_owner_entity();
+	$page_owner = elgg_get_page_owner_entity();
 	if (elgg_instanceof($page_owner, 'group')) {
-    // Only add feedback to a group if it is allowed
-    if (!empty($feedbackgroup) && ($feedbackgroup != 'no')) {
-      if (($page_owner->guid == $feedbackgroup) || (($feedbackgroup == 'grouptool') && ($page_owner->feedback_enable == 'yes')) ) {
-        echo '<div class="elgg-module elgg-module-group elgg-module-group-feedback elgg-module-info">';
-        echo '<div class="elgg-head"><h3>' . elgg_echo('feedback:admin:title') . '</h3></div>';
-        echo '<div class="elgg-body">';
-        echo elgg_view('feedback/list_feedbacks', array());
-        echo '</div>';
-        echo '</div>';
-    	}
-  	}
+		// Only add feedback to a group if it is allowed
+		if (!empty($feedbackgroup) && ($feedbackgroup != 'no')) {
+			if (($page_owner->guid == $feedbackgroup) || (($feedbackgroup == 'grouptool') && ($page_owner->feedback_enable == 'yes')) ) {
+				echo '<div class="elgg-module elgg-module-group elgg-module-group-feedback elgg-module-info">';
+					echo '<div class="elgg-head">';
+						echo '<span class="groups-widget-viewall"><a rel="nofollow" title="' . elgg_echo('feedback:admin:title') . ', ' . elgg_echo('viewall') . '" href="' . $vars['url'] . 'feedback/group/' . $page_owner->guid . '">' . elgg_echo('viewall') . '</a></span>';
+						echo '<h3>' . elgg_echo('feedback:admin:title') . '</h3>';
+					echo '</div>';
+					echo '<div class="elgg-body">';
+						echo elgg_view('feedback/list_feedbacks', array());
+					echo '</div>';
+				echo '</div>';
+			}
+		}
 	}
 }
-
 
