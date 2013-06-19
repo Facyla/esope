6d8e02b3d5b8edce105d20f5f1e53bd63a91fc95
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-06-19 16:17:15 +0200

    Ajouter widget flux RSS pour le profil

---
+++ mod/simplepie/languages/fr.php
@@ -0,0 +1,15 @@
+<?php
+$french = array(
+	'simplepie:widget' => 'Flux RSS',
+	'simplepie:description' => 'Ajoute un flux RSS à votre profil',
+	'simplepie:notset' => 'URL du flux RSS non définie',
+	'simplepie:notfind' => 'Impossible de trouver le flux, vérfier son URL.',
+	'simplepie:feed_url' => 'URL du flux',
+	'simplepie:num_items' => 'Nombre d\'éléments',
+	'simplepie:excerpt' => 'Inclure un extrait',
+	'simplepie:post_date' => 'Inclure la date',
+	'simplepie:postedon' => 'Publié le ',
+);
+
+add_translation("fr", $french);
+
