e46217b6097698cf5fba54fd65f5ef09c3a3e090
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-06-25 13:33:06 +0200

    Light improvements : better output, auto-parse URLs

--- mod/pdf_export/views/default/pdf_export/owner_block_extend.php
+++ mod/pdf_export/views/default/pdf_export/owner_block_extend.php
@@ -1,6 +1,6 @@
 <?php
 if ( $vars['full'] 
-  && !in_array(get_context(), array('listing', 'search')) 
+  && !in_array(elgg_get_context(), array('listing', 'search')) 
   && ($vars['entity'] instanceof ElggObject)
   ) {
   global $CONFIG;
@@ -8,7 +8,7 @@ if ( $vars['full']
   
   //echo '<div class="clearfloat"></div>';
   //  rel="facebox" // pas top quand ça plante
-  echo '<a href="' . $download_pdf_url . '" title="' . elgg_echo('pdfexport:download:title') . '" style="float:right;" class="tooltips-e" target="_new">
+  echo '<a href="' . $download_pdf_url . '" title="' . elgg_echo('pdfexport:download:title') . '" style="float:right; margin-left:8px;" class="tooltips-e" target="_new">
     <img src="' . $CONFIG->url . 'mod/pdf_export/graphics/pdf4_32.png" alt="' . elgg_echo('pdfexport:download:alt') . '" />
     </a>';
   //echo '<div class="clearfloat"></div>';
