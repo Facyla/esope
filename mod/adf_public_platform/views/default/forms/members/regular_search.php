0c8dc89e4c8f3fb5ed130d26f99490709c283ff4
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-06-26 15:13:31 +0200

    Typo bug..

--- mod/adf_public_platform/views/default/forms/members/regular_search.php
+++ mod/adf_public_platform/views/default/forms/members/regular_search.php
@@ -6,6 +6,6 @@
 $params = array('name' => 'q', 'class' => 'mbm');
 echo '<label for="q">' . elgg_echo('members:search') . '</label>';
 echo elgg_view('input/text', $params);
-echo elgg_view('input/hidden', array('name' => 'entity_type', 'value' => 'user');
+echo elgg_view('input/hidden', array('name' => 'entity_type', 'value' => 'user'));
 
 echo elgg_view('input/submit', array('value' => elgg_echo('search')));
