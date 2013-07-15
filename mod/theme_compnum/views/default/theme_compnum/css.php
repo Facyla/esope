1e2c2807883028a649c18dbf26c5f5a154582f22
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-07-15 18:45:21 +0200

    Footer

--- mod/theme_compnum/views/default/theme_compnum/css.php
+++ mod/theme_compnum/views/default/theme_compnum/css.php
@@ -64,7 +64,7 @@ header.notloggedin nav ul li a { background: url("<?php echo $CONFIG->url; ?>mod
 
 
 /* Footer */
-footer { height:auto; }
+footer { height:auto !important; }
 footer * { font-family:Lato; color:white; font-size:16px; font-weight:bold; }
 footer a:hover, footer a:active, footer a:focus { color:white; }
 
