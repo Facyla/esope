b9bbe189ec5b592f0d6a4e5573b06dcbb931e3ef
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-07-15 18:09:43 +0200

    Ajout icônes sociales

--- mod/theme_compnum/views/default/page/elements/footer.php
+++ mod/theme_compnum/views/default/page/elements/footer.php
@@ -21,27 +21,30 @@ $facebook = elgg_get_plugin_setting('facebook', 'adf_public_platform');
 //$flickr = elgg_get_plugin_setting('flickr', 'adf_public_platform');
 $dailymotion = elgg_get_plugin_setting('dailymotion', 'adf_public_platform');
 $slideshare = elgg_get_plugin_setting('slideshare', 'adf_public_platform');
+
+// Construction contenu
+$side_content = '';
+if (!empty($email)) $side_content .= 'CONTACT&nbsp;: ' . $email . '<br />';
+if (!empty($slideshare)) $side_content .= '<a href="' . $slideshare . '" class="contact-slideshare">Slideshare</a> &nbsp; ';
+if (!empty($dailymotion)) $side_content .= '<a href="' . $dailymotion . '" class="contact-dailymotion">Dailymotion</a> &nbsp; ';
+if (!empty($twitter)) $side_content .= '<a href="' . $twitter . '" class="contact-twitter">Twitter</a> &nbsp; ';
+if (!empty($facebook)) $side_content .= '<a href="' . $facebook . '" class="contact-facebook">Facebook</a> &nbsp; ';
+//if (!empty($flickr)) $side_content .= '<a href="' . $flickr . '" class="contact-flickr">FlickR</a> &nbsp; ';
+if (!empty($rss)) $side_content .= '<a href="' . $rss . '" class="contact-rss">RSS</a> &nbsp; ';
 ?>
 
 
 <footer>
 	<div id="theme-compnum-footer">
 		<?php
+		if (!empty($side_content)) {
+			echo '<div style="float:right; width:280px; margin:0 20px;">' . $side_content . '</div>';
+		}
 		echo $footer;
-		echo '<div style="float:right; width:280px; margin:0 20px;">';
-		echo 'CONTACT&nbsp;: ' . $email . '<br />';
-		echo '<a href="" class="contact-slideshare">Slideshare</a> &nbsp; ';
-		echo '<a href="" class="contact-dailymotion">Dailymotion</a> &nbsp; ';
-		echo '<a href="" class="contact-twitter">Twitter</a> &nbsp; ';
-		echo '<a href="" class="contact-facebook">Facebook</a> &nbsp; ';
-		//echo '<a href="" class="contact-flickr">FlickR</a> &nbsp; ';
-		echo '<a href="" class="contact-rss">RSS</a> &nbsp; ';
-		echo '</div>';
 		?>
 	</div>
 </footer>
 
-<div id="bande"></div>
 <div class="interne credits">
 	<p>Conception & réalisation : Facyla ~ <a href="http://www.items.fr/" target="_blank" title="Items International (nouvelle fenêtre)">Items International</a></p>
 	<p class="right">Plateforme construite avec le framework opensource Elgg 1.8</p>
