5fee0b267cbb8014aa41f0414918bf4b2f868f1c
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-07-22 12:13:14 +0200

    Modifs CSS et dossier de preuve (recherche et menu latéral)

--- mod/dossierdepreuve/views/default/dossierdepreuve/search.php
+++ mod/dossierdepreuve/views/default/dossierdepreuve/search.php
@@ -1,13 +1,13 @@
 <?php
 /** Simple dossierdepreuve search */
 global $CONFIG;
-$search = (!empty($vars['search'])) ? $vars['search'] : "Rechercher un dossier";
+$search = (!empty($vars['search'])) ? $vars['search'] : "Nom ou mot du dossier";
 ?>
 <br />
 <div class="sidebarBox">
   <form id="memberssearchform" action="<?php echo $CONFIG->url; ?>search" method="get">
-    <label for="dossierdepreuve-search">Chercher une fiche</label>
-    <input type="text" id="dossierdepreuve-search" name="q" onclick="if (this.value=='Rechercher un dossier') { this.value='' }" class="search_input" value="<?php echo $search; ?>" />
+    <label for="dossierdepreuve-search">Chercher un dossier</label>
+    <input type="text" id="dossierdepreuve-search" name="q" onclick="if (this.value=='Nom ou mot du dossier') { this.value='' }" class="search_input" value="<?php echo $search; ?>" />
     <input type="hidden" name="entity_subtype" value="dossierdepreuve" />
     <input type="hidden" name="entity_type" value="object" />
     <input type="hidden" name="search_type" value="entities" />
--- mod/theme_compnum/views/default/theme_compnum/css.php
+++ mod/theme_compnum/views/default/theme_compnum/css.php
@@ -98,7 +98,7 @@ footer { height:auto !important; background: #7FB622 !important; box-shadow: 0px
 #theme-compnum-footer { margin-top:12px; }
 footer * { font-family:Lato; color:#fafafa; font-size:14px; font-weight:bold; }
 footer a:hover, footer a:active, footer a:focus { color:white; }
-#footer-contacts { float:right; width:250px; margin: 12px 0 16px 30px; padding: 4px 10px; background:#92B025; box-shadow: 0 1px 3px 1px #669966; }
+#footer-contacts { float:right; width:250px; margin: 12px 0 16px 30px; padding: 4px 10px; background:#7FB622; /* ou background:#92B025; box-shadow: 0 1px 3px 1px #669966;*/ box-shadow: 0 1px 3px 1px #666; }
 #footer-contacts * { font-family:LatoLight; }
 #footer-contacts a img { float:left; margin: 0px 2px; opacity:0.8; }
 #footer-contacts a img:hover { opacity:1; }
