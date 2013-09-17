896370f3d939ef8fd83e776f694281746bb71de4
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-09-18 00:50:19 +0200

    Access icons translations added

--- mod/access_icons/languages/fr.php
+++ mod/access_icons/languages/fr.php
@@ -9,17 +9,44 @@ $french = array (
 	'access_icons:settings:helpurl' => "URL de la page d'aide sur les accès",
 	'access_icons:settings:helpurl:help' => "Indiquez l'URL complète de la page qui détaille les divers types d'accès disponibles pour ce site. Cette page sera affichée dans une \"lightbox\" (boîte affichée dans la page), aussi il est préférable qu'elle puisse être affichée sans l'interface du site.<br />Vous pouvez ne rien indiquer pour ne pas afficher de lien du tout sur les niveaux d'accès, ou utiliser le texte configurable ci-dessous à la place.",
 	'access_icons:settings:helptext' => "Texte de la page d'aide sur les accès",
-	'access_icons:settings:helptext:help' => "Plutôt qu'un lien vers une page, vous pouvez configurer ici directement le texte qui apparaîtra pour expliquer les divers types d'accès disponibles pour ce site. Les informations saisies ici seront affichées dans une \"lightbox\" (boîte affichée dans la page). Vous pouvez utiliser tout type de formatage HTML. Laissez le champ vide pour ne rien afficher du tout (pas de lien), ou écrivez \"RAZ\" pour remettre les valeurs par défaut.",
-	'access_icons:settings:helptext:default' => "<table>
-	<tr><th>Niveau d'accès'</th><th>Explications</th></tr>
-	<tr><td><span class=\"elgg-access elgg-access-default\">Par défaut</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-public\">Public</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-members\">Membres du site</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-group\">Groupe</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-collection\">Liste de contacts</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-friends\">Contacts</span></td><td></td></tr>
-	<tr><td><span class=\"elgg-access elgg-access-private\">Privé / Brouillon</span></td><td></td></tr>
-	</table>",
+	'access_icons:settings:helptext:help' => "Plutôt qu'un lien vers une page, vous pouvez configurer ici directement le texte qui apparaîtra pour expliquer les divers types d'accès disponibles pour ce site. Les informations saisies ici seront affichées dans une \"lightbox\" (boîte affichée dans la page). Vous pouvez utiliser tout type de formatage HTML. Laissez le champ vide pour ne rien afficher du tout (pas de lien), ou écrivez \"RAZ\" pour charger les valeurs par défaut.",
+	'access_icons:settings:helptext:default' => "<p>Il est essentiel de d&eacute;finir correctement les droits d'acc&egrave;s lorsque vous faites de nouvelles publications, afin de garantir que les informations que vous publi&eacute;es sont partag&eacute;es avec les bonnes personnes.</p>
+<p>Seules les personnes avec lesquelles vous partagez vos publications y auront acc&egrave;s. Les autres ne sauront pas que vous avez publi&eacute; quelque chose.</p>
+<p>Ainsi, si vous cr&eacute;ez un article en mode \"Priv&eacute;\", personne ne pourra le lire. Inversement, pour qu'une image puisse &ecirc;tre ins&eacute;r&eacute;e dans un article qui sera visible hors connexion, elle doit &ecirc;etre publi&eacute;e en mode \"Public\".</p>
+<p>&nbsp;</p>
+<table>
+<tbody>
+<tr><th>Niveau d'acc&egrave;s</th><th>Explications</th></tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-default\">Par d&eacute;faut</span></td>
+<td>&nbsp;Il ne s'agit pas d'un niveau d'acc&egrave;s au sens propre, mais du <strong>niveau d'acc&egrave;s d&eacute;fini par d&eacute;faut</strong> pour toutes les nouvelles publications. C'est le niveau d'acc&egrave;s qui s'applique pour toute nouvelle publication.</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-public\">Public</span></td>
+<td>&nbsp;La publication est <strong>visible par tout le monde</strong>. Cela inclut tout visiteur du site, y compris les non-membres : il n'est pas n&eacute;cessaire de s'identifier sur le site pour y acc&eacute;der, et votre publication peut &ecirc;tre lue et index&eacute;e par les moteurs de recherche.</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-members\">Membres du site</span></td>
+<td>&nbsp;La publication est visible par <strong>tous les membres</strong> du site, c'est-&agrave;-dire par toute personne qui dispose d'un compte sur la plateforme.</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-group\">Groupe</span></td>
+<td>&nbsp;La publication est visible seulement par&nbsp;les membres d'un<strong> groupe particulier</strong>.</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-collection\">Liste de contacts</span></td>
+<td>&nbsp;La publication est visible par les membres d'une <strong>liste de contacts</strong>. Chaque membre peut cr&eacute;er ses propres listes de contacts, qui sont strictement personnelles (pour une liste partag&eacute;e, il est pr&eacute;f&eacute;rable d'utiliser un groupe).</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-friends\">Contacts</span></td>
+<td>&nbsp;La publication est visible uniquement par les <strong>contacts de l'auteur</strong>.</td>
+</tr>
+<tr>
+<td><span class=\"elgg-access elgg-access-private\">Priv&eacute; / Brouillon</span></td>
+<td>&nbsp;La publication n'est visible que par <strong>vous</strong>.</td>
+</tr>
+</tbody>
+</table>",
 	
 	// Droits d'accès
 	'access_icons:details' => " - Cliquer pour en savoir plus sur tous les droits d'accès disponibles.",
