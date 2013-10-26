3f1e21d8981fbfc5eea6128b9cb645aeb9722ce8
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-10-26 02:27:40 +0200

    Responsive design first steps

--- mod/adf_public_platform/views/default/adf_platform/css.php
+++ mod/adf_public_platform/views/default/adf_platform/css.php
@@ -560,14 +560,19 @@ textarea, .elgg-input-rawtext { width:100%; }
 #groups-tools > li:nth-child(2n+1) { margin-right: 0; margin-left: 0; }
 #groups-tools > li:nth-child(2n) { margin-right: 0; margin-left: 0; }
 
-<<<<<<< HEAD
+/* Generic useful classes */
+.no-spaces { border: 0 none !important; margin: 0 !important; padding: 0 !important; }
+
+
+
+/* Prémices de responsive design */
+
 /* Menu fixé en haut lors du scrolling */
 .floating { position: fixed !important; z-index: 101; }
 header .floating { background:black; width:100%; top:0; height:30px; overflow:hidden; z-index:102; }
 #transverse.floating { top: 30px; }
 
 
-/* Prémices de responsive design */
 /* Pour la fluidité en général */
 .elgg-page-default { min-width:200px; max-width:100%; }
 /* Menus */
@@ -580,6 +585,7 @@ section div.module { width: 94%; padding: 3%; background-size:100%; }
 section div.module, section div.module div.activites { min-width:180px; width:auto; }
 section div.module footer { background-size: 100%; }
 
+
 @media (max-width:1225px) {
 	.interne { max-width:980px; }
 }
@@ -587,10 +593,4 @@ section div.module footer { background-size: 100%; }
 @media (max-width:980px) {
 	.interne { max-width:98%; }
 }
-=======
-
-/* Generic useful classes */
-.no-spaces { border: 0 none !important; margin: 0 !important; padding: 0 !important; }
-
->>>>>>> bc83403516a187cb9f8136faacdc6adbf649a070
 
