43693be0a0668a94c7775c9d03fa8c01774051ce
Facyla ~ Florian DANIEL <facyla@gmail.com>
2013-11-13 13:00:17 +0100

    Filter supported editable mimetypes (list should be completed or configured via plugin settings)

--- mod/elgg_cmis/lib/elgg_cmis.php
+++ mod/elgg_cmis/lib/elgg_cmis.php
@@ -10,6 +10,10 @@ function elgg_cmis_list_objects($objs = false, $debug = false) {
 			'page' => 'page/document-details?nodeRef=workspace://',
 			'edit' => 'page/inline-edit?nodeRef=workspace://',
 		);
+	$editable_mimetypes = array(
+			'text/plain',
+			'text/html',
+		);
 	$return = '';
 	
 	if ($objs) foreach ($objs->objectList as $obj) {
@@ -17,15 +21,18 @@ function elgg_cmis_list_objects($objs = false, $debug = false) {
 		switch($obj->properties['cmis:baseTypeId']) {
 			// DOCUMENT
 			case 'cmis:document':
+				$return .=  '<p>';
+				$mimetype = $obj->properties['cmis:contentStreamMimeType'];
 				$return .= elgg_echo('elgg_cmis:icon:document') . $obj->properties['cmis:name'] . ' &nbsp; ';
 				$fileID = str_replace('workspace://', '', $obj->properties['alfcmis:nodeRef']);
 				foreach ($action_type as $action => $action_path) {
 					$action_url = $file_baseurl . $action_path . $fileID;
 					if ($action == 'download') $action_url .= '?a=true';
+					if (($action == 'edit') && !in_array($mimetype, $editable_mimetypes)) continue;
 					$return .=  '<a href="' . $action_url . '" target="_new" title="' . elgg_echo('elgg_cmis:action:'.$action) . '">' . elgg_echo('elgg_cmis:icon:'.$action) . '</a>';
 				}
 				if ($debug) $return .= print_r($obj);
-				$return .=  '<br />';
+				$return .=  '</p>';
 				break;
 			
 			// DOSSIER
