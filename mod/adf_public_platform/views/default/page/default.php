f950dfb5ba46a4bb85453d6e1eff59ece314b3f5
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-01-08 18:04:48 +0100

    Thème Inria

--- mod/adf_public_platform/views/default/page/default.php
+++ mod/adf_public_platform/views/default/page/default.php
@@ -35,6 +35,8 @@ $footer = elgg_view('page/elements/footer', $vars);
 
 // Set the content type
 header("Content-type: text/html; charset=UTF-8");
+// Allow external embed (hack)
+header('X-Frame-Options: GOFORIT'); 
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
