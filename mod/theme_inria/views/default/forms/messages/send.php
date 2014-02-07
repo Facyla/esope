9017a2c9363d95c764e785571d9bb26fa5c4c111
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-02-07 17:24:28 +0100

    Thème Inria

--- mod/theme_inria/views/default/forms/messages/send.php
+++ mod/theme_inria/views/default/forms/messages/send.php
@@ -11,7 +11,7 @@ $subject = elgg_extract('subject', $vars, '');
 $body = elgg_extract('body', $vars, '');
 
 $recipients_options = array();
-$vars['friends'] = $page_owner->getFriends('', false);
+$vars['friends'] = elgg_get_page_owner_entity()->getFriends('', false);
 foreach ($vars['friends'] as $friend) {
 	$recipients_options[$friend->guid] = $friend->name;
 }
