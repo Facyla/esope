2dabd536a0d94a2a614b149fb2f6692082468c86
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-03-10 20:49:18 +0100

    PDF export translated into EN

---
+++ mod/pdf_export/languages/en.php
@@ -0,0 +1,29 @@
+<?php
+$english = array(
+	
+	'pdfexport'	=>	"PDF export",
+	'pdfexport:download:alt' => "Download as PDF",
+	'pdfexport:download:title' => "Download this content as a PDF file",
+	
+	'pdfexport:publishedby' => "Published by ",
+	'pdfexport:publishedin' => "Published in ",
+	'pdfexport:publisheddate' => "on ",
+	'pdfexport:generated' => "Page generated on  ",
+	'pdfexport:generated:format' => "d/m/Y at H:i",
+	'pdfexport:entitydate:format' => "d/m/Y",
+	'pdfexport:lastupdated' => "last updated on ",
+	'pdfexport:tags' => "Tags",
+	
+	'pdfexport:error:guid' => "No page to export, or invalid GUID",
+	'pdfexport:error:badtype' => 'Cannot export this type of entity',
+	'pdfexport:error:user' => "Cannot export site members.",
+	'pdfexport:error:group' => "Cannot export groupes.",
+	'pdfexport:error:site' => "Cannot export sites.",
+	'pdf_export:validsubtypes' => "Content types (subtypes) for which  PDF export is enabled. Clear list to restore default values.",
+	'pdf_export:validsubtypes:default' => "Default list: %s",
+	
+	
+);
+
+add_translation('en', $english);
+
--- mod/pdf_export/languages/fr.php
+++ mod/pdf_export/languages/fr.php
@@ -1,25 +1,27 @@
 <?php
 $french = array(
-  
-  'pdfexport'  =>  "Export PDF",
-  'pdfexport:download:alt' => "Télécharger en PDF",
-  'pdfexport:download:title' => "Télécharger cette publication au format PDF",
-  
-  'pdfexport:publishedby' => "Publié par ",
-  'pdfexport:publishedin' => "Publié dans ",
-  'pdfexport:publisheddate' => "le ",
-  'pdfexport:generated' => "Page générée le ",
-  'pdfexport:generated:format' => "d/m/Y à H:i",
-  'pdfexport:entitydate:format' => "d/m/Y",
-  
-  'pdfexport:error:guid' => "Pas de page à exporter ou GUID invalide",
-  'pdfexport:error:badtype' => 'Export impossible pour ce type d\'entité.',
-  'pdfexport:error:user' => "Export des membres du site impossible.",
-  'pdfexport:error:group' => "Export des groupes impossible.",
-  'pdfexport:error:site' => "Export des espaces impossible.",
-  'pdf_export:validsubtypes' => "Types de publications (subtypes) pour lesquelles un export PDF est disponible. Effacer pour récupérer la liste par défaut.",
-  'pdf_export:validsubtypes:default' => "Liste par défaut : %s",
-  
+	
+	'pdfexport'	=>	"Export PDF",
+	'pdfexport:download:alt' => "Télécharger en PDF",
+	'pdfexport:download:title' => "Télécharger cette publication au format PDF",
+	
+	'pdfexport:publishedby' => "Publié par ",
+	'pdfexport:publishedin' => "Publié dans ",
+	'pdfexport:publisheddate' => "le ",
+	'pdfexport:generated' => "Page générée le ",
+	'pdfexport:generated:format' => "d/m/Y à H:i",
+	'pdfexport:entitydate:format' => "d/m/Y",
+	'pdfexport:lastupdated' => "dernière mise à jour le ",
+	'pdfexport:tags' => "Tags ",
+	
+	'pdfexport:error:guid' => "Pas de page à exporter ou GUID invalide",
+	'pdfexport:error:badtype' => "Export impossible pour ce type d'entité.",
+	'pdfexport:error:user' => "Export des membres du site impossible.",
+	'pdfexport:error:group' => "Export des groupes impossible.",
+	'pdfexport:error:site' => "Export des espaces impossible.",
+	'pdf_export:validsubtypes' => "Types de publications (subtypes) pour lesquelles un export PDF est disponible. Effacer pour récupérer la liste par défaut.",
+	'pdf_export:validsubtypes:default' => "Liste par défaut : %s",
+	
 );
 
 add_translation('fr', $french);
