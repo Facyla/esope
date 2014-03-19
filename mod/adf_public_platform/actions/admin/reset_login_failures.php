b38b67a695e44acccf8b2cf0b58169d76c962986
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-03-19 23:33:42 +0100

    Add reset login failures actions (no visible interface yet)

--- mod/adf_public_platform/actions/admin/reset_login_failures.php
+++ mod/adf_public_platform/actions/admin/reset_login_failures.php
@@ -9,7 +9,7 @@
 if (elgg_is_admin_logged_in()) {
 	$guid = get_input('guid');
 	$user = get_entity($guid);
-	if (reset_login_failure_count($user_guid)) {
+	if (reset_login_failure_count($user->guid)) {
 		system_message(elgg_echo('admin:user:unban:yes'));
 	} else {
 		register_error(elgg_echo('admin:user:unban:no'));
