732db3e9ff2ae9328da03df8c1c5fafffc84717b
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-04-29 13:02:29 +0200

    Extend online members view capabilities : config + max set to 60

--- mod/adf_public_platform/views/default/adf_platform/users/online.php
+++ mod/adf_public_platform/views/default/adf_platform/users/online.php
@@ -1,9 +1,14 @@
-<?php $newest_members = find_active_users(1800, 18, 0); ?>
+<?php
+// Timeframe : 30 minutes (30*60)
+// Max : 50
+$max = elgg_extract('limit', $vars, 60);
+$duration = elgg_extract('duration', $vars, 1800);
+$online_members = find_active_users($duration, $max, 0); ?>
 
 <div class="sidebarBox">
-  <?php
-  echo '<h3><a href="' . $vars['url'] . 'members/online">' . elgg_echo('adf_platform:members:online') .'</a></h3>';
-  echo elgg_view('adf_platform/users/members', array('members' => $newest_members));
-  ?>
+	<?php
+	echo '<h3><a href="' . $vars['url'] . 'members/online">' . elgg_echo('adf_platform:members:online') .'</a></h3>';
+	echo elgg_view('adf_platform/users/members', array('members' => $online_members));
+	?>
 </div>
 
