e988b63165b229538b5cecb198fcc88f8440b288
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-15 20:51:38 +0200

    Add tests to LDAP

--- mod/ldap_auth2/classes/LdapServer.php
+++ mod/ldap_auth2/classes/LdapServer.php
@@ -153,6 +153,7 @@ class LdapServer {
 			//if ((count($attributes) < 1) && ($attributes != 'rdn')) $attributes = ldap_get_attributes($this->getLink(), $entry);
 			if ($attributes[0] == "*") {
 				$attributes = ldap_get_attributes($this->getLink(), $entry);
+				if (is_array($attributes)) { $test = print_r($attributes, true); error_log($test); }
 				//$attributes = array_pop($attributes);
 			}
 			while ($entry) {
