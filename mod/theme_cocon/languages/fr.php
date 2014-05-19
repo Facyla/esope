236f1bdd8593771361580de57036882c58838434
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-20 00:26:46 +0200

    Cocon

--- mod/theme_cocon/languages/fr.php
+++ mod/theme_cocon/languages/fr.php
@@ -15,6 +15,8 @@ $french = array(
 	'theme_cocon:home:edito' => "Edito",
 	'theme_cocon:home:information' => "Informations",
 	'theme_cocon:home:widgets' => "Widgets",
+	'theme_cocon:site:activity' => "Activité récente",
+	'members:online' => "Membres connectés (%s)",
 	
 	'theme_cocon:groups:featured' => "Groupes en Une",
 	'theme_cocon:members:newest' => "Nouveaux membres",
