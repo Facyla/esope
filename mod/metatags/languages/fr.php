c29e2143441487988a49909510bd64670f5d5236
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-20 21:31:11 +0200

    Ajout metatags

---
+++ mod/metatags/languages/fr.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Help text metatags description and title
+ *
+ * @package Metatags
+ */
+
+$french = array(
+	//Mainpage settings
+	'metatags:mainpage:title' => "Titre d'accroche pour la page d'accueil",
+	'metatags:mainpage:keywords' => "Fournissez quelques mots-clefs qui décrivent votre site et sont mentionnés sur la page d'accueil",
+	'metatags:mainpage:description' => "Fournissez une description d'accroche pour votre réseau. Astuce : utilisez les mots présents sur la page principale pour de meilleurs résultats.",
+	'metatags:mainpage:image' => "Indiquez le nom de fichier d'une image représentant votre site. Le fichier doit être situé dans le dossier /_graphics",
+	'metatags:cloudflare' => "Souhaitez-vous utiliser les URL d'avatar stables ?",
+
+	//settings voor plugins
+	'blog:message:description' => "a publié un nouvel article",
+	'blog:title:preformat' => "Blog - %s",
+	'market:message:description' => "souhaite partager cette offre",
+	'market:title:preformat' => "%s - publicité",
+		
+);
+
+add_translation("fr", $french);
+
