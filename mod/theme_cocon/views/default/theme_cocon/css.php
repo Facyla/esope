dced644f486f9ee4eff530d612f8368a27a45f61
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-21 12:33:16 +0200

    Cocon

--- mod/theme_cocon/views/default/theme_cocon/css.php
+++ mod/theme_cocon/views/default/theme_cocon/css.php
@@ -25,18 +25,21 @@ html, body { background: url(<?php echo $imgurl; ?>background.jpg) top left no-r
 header { background: transparent; border-top:0; height:120px; }
 header h1 { margin-top: 17px; }
 header h1:hover { text-decoration:none; }
-header nav ul li { }
+header nav ul li { margin-right:7px; }
 header nav ul li a { width: 30px; height: 30px; background: white !important; border-radius: 16px; line-height: 30px; text-align: center; font-size: 18px; color:#2a7d9f; text-shadow:none; padding: 0 !important; display: inline-block; }
 header nav a .fa { margin-right: 0; }
-#user img { border-radius: 13px; width: 26px; padding-top: 2px; }
+#user img { border-radius: 14px; width: 28px; padding-top: 1px; }
 #transverse { box-shadow:none; border-bottom:0; background:white; height:40px; }
 #transverse nav ul li { border-right:0; }
 #transverse nav ul li:first-child { border-left:0; }
-#transverse nav ul li a { color:#2a7d9f; padding: 12px 10px 9px 10px; font-size: 17px; }
+#transverse nav ul li a { color:#2a7d9f; line-height: 40px; padding: 0; font-size: 17px; }
 #transverse nav ul li li a { font-weight:normal; }
 /* Search form */
 #transverse form { border: 0; }
-::-webkit-input-placeholder, :-moz-placeholder, ::-moz-placeholder, :-ms-input-placeholder { color:#2a7d9f; }
+::-webkit-input-placeholder { color:#2a7d9f; }
+:-moz-placeholder { color:#2a7d9f; }
+::-moz-placeholder { color:#2a7d9f; }
+:-ms-input-placeholder { color:#2a7d9f; }
 form input#adf-search-input, form input#adf-search-input:active, form input#adf-search-input:focus { color:#2a7d9f; background:white; }
 form input#adf-search-submit-button, form input#adf-search-submit-button:active, form input#adf-search-submit-button:focus { background:white; border:0; }
 
@@ -95,5 +98,6 @@ a.elgg-button { border-width: 2px; border-radius: 12px; }
 color: white; display: inline-block; margin: 2px 6px; }
 
 /* Users and avatars */
-.elgg-avatar-tiny > a > img { border-radius: 20px; border: 1px solid white; }
+.elgg-avatar-tiny > a > img { border-radius: 10px; border: 1px solid white; }
+.elgg-avatar-small > a > img { border-radius: 20px; border: 1px solid white; }
 
