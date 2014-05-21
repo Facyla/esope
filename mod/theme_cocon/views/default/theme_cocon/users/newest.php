0c8455bb3258c1f1b3e5f0fee1d951f643b8dec9
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-21 22:06:41 +0200

    Cocon

--- mod/theme_cocon/views/default/theme_cocon/users/newest.php
+++ mod/theme_cocon/views/default/theme_cocon/users/newest.php
@@ -2,7 +2,7 @@
 ?>
 
 <div class="sidebarBox">
-	<h3><a href="<?php echo $vars['url']; ?>members/newest"><i class="fa fa-users"></span> <?php echo elgg_echo('theme_cocon:members:newest') ?></a></h3>
+	<h3><a href="<?php echo $vars['url']; ?>members/newest"><i class="fa fa-user"></i> <?php echo elgg_echo('theme_cocon:members:newest') ?></a></h3>
 	<?php echo elgg_view('theme_cocon/users/members', array('members' => $newest_members)); ?>
 </div>
 
