09c468b1c2da50271a49c346b112d0c2262038b2
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-21 12:34:16 +0200

    Cocon

--- mod/theme_cocon/views/default/theme_cocon/css.php
+++ mod/theme_cocon/views/default/theme_cocon/css.php
@@ -32,7 +32,7 @@ header nav a .fa { margin-right: 0; }
 #transverse { box-shadow:none; border-bottom:0; background:white; height:40px; }
 #transverse nav ul li { border-right:0; }
 #transverse nav ul li:first-child { border-left:0; }
-#transverse nav ul li a { color:#2a7d9f; line-height: 40px; padding: 0; font-size: 17px; }
+#transverse nav ul li a { color:#2a7d9f; line-height: 40px; padding: 0 10px; font-size: 17px; }
 #transverse nav ul li li a { font-weight:normal; }
 /* Search form */
 #transverse form { border: 0; }
