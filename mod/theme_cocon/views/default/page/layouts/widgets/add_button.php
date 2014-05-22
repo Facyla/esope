265506d9758a6cf51a3007fa9478188aaca2ef92
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-05-23 01:09:04 +0200

    Cocon

--- mod/theme_cocon/views/default/page/layouts/widgets/add_button.php
+++ mod/theme_cocon/views/default/page/layouts/widgets/add_button.php
@@ -3,14 +3,14 @@
  * Button area for showing the add widgets panel
  */
 global $CONFIG;
-$imgurl = $CONFIG->url . 'mod/theme_cocon/graphics/';
+$picto_url = $CONFIG->url . 'mod/theme_cocon/graphics/pictos/';
 ?>
 <div class="elgg-widget-add-control">
 	<div class="cocon-widget-add-control">
-		<?php
+		<img src="<?php echo $picto_url; ?>widgets_edit.png" />&nbsp;<?php
 			echo elgg_view('output/url', array(
 				'href' => '#widgets-add-panel',
-				'text' => '<img src="' . $imgurl . 'pictos/widgets_edit.png" />&nbsp;' . elgg_echo('widgets:add'),
+				'text' => elgg_echo('widgets:add'),
 				'class' => 'cocon-widget-add-button',
 				'rel' => 'toggle',
 			));
