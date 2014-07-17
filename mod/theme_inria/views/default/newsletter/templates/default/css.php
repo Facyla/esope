aa236dde6ed32c0327693bac3f3067439b26e7f4
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-07-17 22:34:41 +0200

    Modif template par défaut newsletters Iris

---
+++ mod/theme_inria/views/default/newsletter/templates/default/css.php
@@ -0,0 +1,123 @@
+<?php
+global $CONFIG;
+?>
+
+body {
+	background: #f6f6f6;
+	color: #333333;
+	font: 80%/1.4 "Lucida Grande",Arial,Tahoma,Verdana,sans-serif;
+	word-wrap: break-word;
+	-moz-hyphens: auto;
+	-webkit-hyphens: auto;
+	-ms-hyphens: auto;
+	-o-hyphens: auto;
+	hyphens: auto;
+}
+
+a {
+	color: #6D2D4F;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
+
+img {
+	border: none;
+}
+h1, h2, h3, h4 {
+	color: #EF783E;
+	margin: 0;
+}
+
+h1 {
+	font-size: 18px;
+}
+
+h2 {
+	font-size: 16px;
+}
+
+h3 {
+	font-size: 16px;
+}
+
+h4 {
+	font-size: 14px;
+}
+
+#newsletter_online {
+	font-size: 11px;
+	color: #999999;
+	text-align: center;
+	padding: 10px 20px 0px;
+	margin: 0 auto;
+	width: 600px;
+}
+
+#newsletter_header {
+	background: #6D2D4F;
+	padding: 10px 30px;
+	min-height: 20px;
+	
+	border: 1px solid #6D2D4F;
+	-webkit-border-radius: 5px 5px 0 0;
+	-moz-border-radius: 5px 5px 0 0;
+	border-radius: 5px 5px 0 0;
+}
+
+#newsletter_header h1{
+	color: #FFFFFF;
+}
+
+#newsletter_container {
+	padding: 20px 0;
+	width: 600px;
+	margin: 0 auto;
+}
+
+#newsletter_unsubscribe {
+	font-size: 11px;
+	color: #999999;
+	padding: 20px;
+	text-align: center;
+}
+
+#newsletter_footer {
+	background: #F0F0F0;
+	
+	border-top: 1px solid #FFFFFF;
+	border-left: 1px solid #dbdbdb;
+	border-bottom: 1px solid #dbdbdb;
+	border-right: 1px solid #dbdbdb;
+	
+	-webkit-border-radius: 0 0 5px 5px;
+	-moz-border-radius: 0 0 5px 5px;
+	border-radius: 0 0 5px 5px;
+	
+	padding: 5px;
+	text-align: right;
+}
+#newsletter_footer img { height:35px; }
+
+.elgg-module-newsletter {
+	background: #FFFFFF;
+	padding: 30px;
+	
+	border-top: 1px solid #FFFFFF;
+	border-left: 1px solid #dbdbdb;
+	border-bottom: 1px solid #dbdbdb;
+	border-right: 1px solid #dbdbdb;
+}
+
+.elgg-module-newsletter .elgg-head {
+	padding-bottom: 5px;
+	border-bottom: 1px solid #dbdbdb;
+}
+
+.elgg-module-newsletter h1 a,
+.elgg-module-newsletter h2 a,
+.elgg-module-newsletter h3 a {
+	text-decoration: none;
+}
