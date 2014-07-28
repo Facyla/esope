e49aae7b7baa3058964011068666afd1b652e92b
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-07-28 12:58:48 +0200

    Plugin tempalte

---
+++ mod/plugin_template/start.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * plugin_template plugin
+ *
+ */
+
+elgg_register_event_handler('init', 'system', 'plugin_template_init'); // Init
+
+
+/**
+ * Init plugin_template plugin.
+ */
+function plugin_template_init() {
+	global $CONFIG; // All site useful vars
+	
+	//$messages_send = elgg_get_plugin_setting('messages_send', 'plugin_template');
+
+	
+}
+
+
+// Other useful functions
+// prefixed by plugin_name_
+/*
+function plugin_template_function() {
+	
+}
+*/
+
+
+
