9a04b9279da22dcc24a08f26f110295d3e5934c1
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-08-01 20:03:05 +0200

    Add template plugin and theme

--- mod/plugin_template/start.php
+++ mod/plugin_template/start.php
@@ -4,7 +4,8 @@
  *
  */
 
-elgg_register_event_handler('init', 'system', 'plugin_template_init'); // Init
+// Init plugin
+elgg_register_event_handler('init', 'system', 'plugin_template_init');
 
 
 /**
@@ -13,9 +14,35 @@ elgg_register_event_handler('init', 'system', 'plugin_template_init'); // Init
 function plugin_template_init() {
 	global $CONFIG; // All site useful vars
 	
-	//$messages_send = elgg_get_plugin_setting('messages_send', 'plugin_template');
-
+	// Get a plugin setting
+	$setting = elgg_get_plugin_setting('setting_name', 'plugin_template');
 	
+	// Get a user plugin setting (makes sense only if logged in)
+	if (elgg_is_logged_in()) {
+		$user_guid = elgg_get_logged_in_user_guid();
+		$usersetting = elgg_get_plugin_user_setting('user_plugin_setting', $user_guid, 'plugin_template');
+	}
+	
+	// Register a page handler on "plugin_template/"
+	elgg_register_page_handler('plugin_template', 'plugin_template_page_handler');
+	
+	
+}
+
+
+// Page handler
+// Loads pages located in plugin_template/pages/plugin_template/
+function plugin_template_page_handler($page) {
+	$base = elgg_get_plugins_path() . 'plugin_template/pages/plugin_template';
+	switch ($page[0]) {
+		case 'view':
+			set_input('guid', $page[1]);
+			include "$base/example_page.php";
+			break;
+		default:
+			include "$base/example_page.php";
+	}
+	return true;
 }
 
 
