8a2a6a4ad5af0e8092c0b157c0b04d4c2fdce3f7
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-08-01 19:37:07 +0200

    Folders links

---
+++ mod/plugin_template/page/plugin_template/example_page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Elgg output page content
+* 
+* @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+* @author Facyla
+* @copyright Facyla 2010-2014
+* @link http://id.facyla.fr/
+*/
+
+// Load Elgg engine
+global $CONFIG;
+
+$title = "Title";
+
+$body = "Plugin_tempalte page content";
+
+
+// Render the page
+echo elgg_view_page($title, $body);
+
+
