3aa92441c57a0b90d37a282bb7b4858c80c8e101
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-08-28 00:51:37 +0200

    Add block to display info about comments (public)

---
+++ mod/adf_public_platform/views/default/comments/public_notice.php
@@ -0,0 +1,8 @@
+<?php
+/* Copy and tweak in your custom theme depending on your exact needs and registration policy
+if (!elgg_is_logged_in()) {
+	echo '<div class="clearfloat"></div><br />';
+	// echo "<blockquote><i class=\"fa fa-info-circle\"></i> You need to be logged in to comment. Please <a href=\"" . $vars['url'] . "login\">login</a>, or <a href=\"" . $vars['url'] . "register\">create an account</a>.</blockquote>";
+}
+*/
+
