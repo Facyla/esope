f3359106793eb1baa90c66a6c34b220e3a086762
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-10-13 21:10:00 +0200

    Etherpad

--- mod/elgg_etherpad/views/default/elgg_etherpad/css.php
+++ mod/elgg_etherpad/views/default/elgg_etherpad/css.php
@@ -1,3 +1,3 @@
-.elgg-context-pad .elgg-form fieldset { border:1px solid; margin:1ex 0; padding:1ex; }
+.elgg-context-pad .elgg-form fieldset { border:1px solid; margin:1ex 0 3ex; padding:1ex 1ex 2ex 1ex; }
 .elgg-context-pad .elgg-form fieldset legend { padding:0 1ex; }
 
