fb6665ac71c9594d384894f68e3a33ab6374c809
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-11-05 18:57:46 +0100

    Inactive group styles

--- mod/theme_inria/views/default/theme_inria/css.php
+++ mod/theme_inria/views/default/theme_inria/css.php
@@ -255,7 +255,7 @@ section .interne div.module div.activites { background-color: <?php echo $module
 
 /* Anciens groupes */
 .inria-group-oldactivity { border:1px dotted black; background:yellow; padding:1ex 3ex;; margin: 1ex 0; text-align:center; }
-.group-oldactivity { background: rgba(255,255,0,0.6); font-size: 6px; padding: 2px 0px; }
+.group-oldactivity { display:block; left:0; position: absolute; top:0; width: 100%; text-align:center; border:0; color:black; }
 .group-oldactivity-tiny { background: rgba(255,255,0,0.6); font-size: 6px; padding: 2px 0px; }
 .group-oldactivity-small { background: rgba(255,255,0,0.8); font-size: 8px; padding: 3px 1px; }
 .group-oldactivity-medium { background: rgba(255,255,0,0.8); font-size: 10px; padding: 3px 1px; }
