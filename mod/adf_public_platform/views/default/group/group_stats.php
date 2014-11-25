c1ceb1fe12527d2159d006097d06a02a01475628
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-11-25 17:37:37 +0100

    ignore access rights when determining group activity

--- mod/adf_public_platform/views/default/group/group_stats.php
+++ mod/adf_public_platform/views/default/group/group_stats.php
@@ -10,6 +10,8 @@ if (!empty($timeframe) && is_int($timeframe)) {
 	$timeframe =  time() - (180 * 24 * 60 * 60);
 }
 
+$ia = elgg_get_ignore_access();
+elgg_set_ignore_access(true);
 $latest_river = elgg_get_river(array(
 		'limit' => 1,
 		'joins' => array("JOIN {$dbprefix}entities e1 ON e1.guid = rv.object_guid"),
@@ -24,7 +26,7 @@ $latest_river = elgg_get_river(array(
 if ($group->time_created < $timeframe) {
 	// Si pas de contenu ou si le dernier contenu est plus ancien qu'un certain temps => on prévient...
 	if (!$latest_river || ($latest_river[0]->posted < $timeframe)) {
-		echo '<div class="inria-group-oldactivity">';
+		echo '<div class="group-oldactivity">';
 		$latest_action = elgg_view_friendly_time($latest_river[0]->posted);
 		if ($latest_river[0]->posted > 0) echo '<i class="fa fa-warning"></i> ' . elgg_echo('esope:group:oldactivity', array($latest_action));
 		else  echo '<i class="fa fa-warning"></i> ' . elgg_echo('esope:group:norecentactivity');
@@ -32,4 +34,6 @@ if ($group->time_created < $timeframe) {
 		echo '</div>';
 	}
 }
+
+elgg_set_ignore_access($ia);
 
