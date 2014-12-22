aa6d626609f38f0556e910dd79533cf6d6d78b37
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-12-22 19:57:09 +0100

    FA icons

--- mod/adf_public_platform/views/default/css/elements/navigation.php
+++ mod/adf_public_platform/views/default/css/elements/navigation.php
@@ -831,7 +831,7 @@ EOT;
 /* Note : this replaces the above with FA icons - update translations accordingly if needed
  */
 .elgg-menu-owner-block li a { padding-left: 1ex; }
-.elgg-sidebar li .fa { display: inline-block; min-width: 3ex; }
+.elgg-sidebar li .fa { display: inline-block; min-width: 2.5ex; }
 
 
 
