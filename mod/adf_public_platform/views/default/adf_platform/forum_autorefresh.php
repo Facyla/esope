1e74e0d3a9952752f26047c6d70d61922e49d37f
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-01-16 10:04:35 +0100

    Enable autorefresh setting

--- mod/adf_public_platform/views/default/adf_platform/forum_autorefresh.php
+++ mod/adf_public_platform/views/default/adf_platform/forum_autorefresh.php
@@ -3,6 +3,11 @@
 $auto_refresh = get_input('autorefresh');
 if ($auto_refresh != 'auto') { return; }
 
+// Refresh rate : floor to 10 seconds
+$auto_refresh_rate = get_input('rate', 20);
+if (!is_int($auto_refresh_rate) || ($auto_refresh_rate < 10)) { $auto_refresh_rate = 20; }
+$auto_refresh_rate = $auto_refresh_rate * 1000;
+
 $refresh_url = elgg_get_site_url() . 'esope/forum_refresh/' . $vars['entity']->guid;
 // echo '<a href="javascript:void(0);" onclick="forum_refresh();">Refresh</a>';
 // echo "Load URL : $refresh_url";
@@ -40,7 +45,8 @@ $(document).ready(function() {
 	$('#group-replies .elgg-pagination').hide();
 	//$('#group-replies .elgg-annotation-list').html('<i class="fa fa-spinner fa-spin></i>').fadeOut("slow");
 	forum_refresh();
-	var auto_refresh = setInterval(function() { forum_refresh(); }, 20000); // Timeout de 10 secondes
+	// Autorefresh de 20 secondes par défaut
+	var auto_refresh = setInterval(function() { forum_refresh(); }, <?php echo $auto_refresh_rate; ?>);
 	//$('#group-replies').prepend('<div id="annotations-loading" style="display:none;">Loading content</div>');
 });
 </script>
