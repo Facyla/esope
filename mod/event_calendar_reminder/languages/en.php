70778f93a74573fe6e403d0b51b933f74cb2ba36
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-02-13 23:13:48 +0100

    Translation and email content

--- mod/event_calendar_reminder/languages/en.php
+++ mod/event_calendar_reminder/languages/en.php
@@ -1,5 +1,9 @@
 <?php
-$language = array (
+$language = array(
+	
+	'event_calendar_reminder:setting:usersettings' => "Enable user settings (disable reminders)",
+	'event_calendar_reminder:setting:reminder_days' => "Reminders days (eg.: 1,3,7)",
+	
 	'event_calendar_reminder:subject' => 'Event reminder: %s',
 	'event_calendar_reminder:message' => 'The event <b>%s</b> you have subscribed to will happen on <b>%s</b
 %s
--- mod/event_calendar_reminder/languages/fr.php
+++ mod/event_calendar_reminder/languages/fr.php
@@ -1,5 +1,9 @@
 <?php
-$language = array (
+$language = array(
+	
+	'event_calendar_reminder:setting:usersettings' => "Permettre aux membres de gérer leurs propres paramètres (désactivation des envois)",
+	'event_calendar_reminder:setting:reminder_days' => "Envoyer un rappel X jours avant la date (par ex.: 1,3,7)",
+	
 	'event_calendar_reminder:subject' => "Rappel d'agenda : %s",
 	'event_calendar_reminder:message' => "L'événement <b>%s</b> auquel vous êtes inscrit aura lieu le <b>%s</b
 %s
