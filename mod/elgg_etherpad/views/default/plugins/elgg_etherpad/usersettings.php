b58d75ac75c2384248b546c326750babd59de082
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-02-15 22:40:26 +0100

    Etherpad usersettings

--- mod/elgg_etherpad/views/default/plugins/elgg_etherpad/usersettings.php
+++ mod/elgg_etherpad/views/default/plugins/elgg_etherpad/usersettings.php
@@ -1,4 +1,6 @@
 <?php
+return;
+
 global $CONFIG;
 
 $url = $vars['url'];
@@ -6,6 +8,7 @@ $url = $vars['url'];
 // Define dropdown options
 $yes_no_opt = array( 'yes' => elgg_echo('option:yes'), 'no' => elgg_echo('option:no') );
 
+/*
 
 // Set default value
 if (!isset($vars['entity']->user_plugin_setting)) { $vars['entity']->user_plugin_setting == 'default'; }
@@ -14,8 +17,9 @@ if (!isset($vars['entity']->user_plugin_setting)) { $vars['entity']->user_plugin
 // Example yes/no setting
 echo '<p><label>Test select setting "user_plugin_setting"</label> ' . elgg_view('input/dropdown', array('name' => 'params[user_plugin_setting]', 'options_values' => $yes_no_opt, 'value' => $vars['entity']->user_plugin_setting)) . '</p>';
 
-
 // Example text setting
 echo '<p><label>Text setting "user_plugin_setting2"</label> ' . elgg_view('input/dropdown', array('name' => 'params[user_plugin_setting2]', 'value' => $vars['entity']->user_plugin_setting2)) . '</p>';
+
+*/
 
 
