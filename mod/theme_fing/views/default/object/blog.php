4efc77d6bc50ce0920706371202f4221cbed88d4
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-03-04 20:17:06 +0100

    Blog titles

--- mod/theme_fing/views/default/object/blog.php
+++ mod/theme_fing/views/default/object/blog.php
@@ -95,8 +95,10 @@
 			'value' => $blog->description,
 			'class' => 'blog-post',
 		));
-
-		$header = elgg_view_title($blog->title);
+		
+		// Note : title may appear in full view, even if some redundancy (but we need it in listings !)
+		//$header = elgg_view_title($blog->title);
+		$header = "<h3>" . elgg_view("output/url", array("text" => $blog->title, "href" => $blog->getURL())) . "</h3>";
 
 		$params = array(
 			'entity' => $blog,
