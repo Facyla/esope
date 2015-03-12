8ae339b536a6964cb4bffba0a19121eabc1e110e
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-03-12 18:07:33 +0100

    EN translation

--- mod/survey/languages/en.php
+++ mod/survey/languages/en.php
@@ -6,74 +6,74 @@ $en = array(
 	/**
 	 * Menu items and titles
 	 */
-	'poll' => "Polls",
-	'survey:add' => "New Poll",
-	'survey:group_poll' => "Group polls",
-	'survey:group_poll:listing:title' => "%s's polls",
-	'survey:your' => "Your polls",
-	'survey:not_me' => "%s's polls",
-	'survey:friends' => "Friends' polls",
-	'survey:addpost' => "Create a poll",
-	'survey:editpost' => "Edit a poll: %s",
-	'survey:edit' => "Edit a poll",
-	'item:object:poll' => 'Polls',
-	'item:object:poll_choice' => "Poll choices",
-	'survey:question' => "Poll question",
+	'survey' => "Surveys",
+	'survey:add' => "New Survey",
+	'survey:group_survey' => "Group surveys",
+	'survey:group_survey:listing:title' => "%s's surveys",
+	'survey:your' => "Your surveys",
+	'survey:not_me' => "%s's surveys",
+	'survey:friends' => "Friends' surveys",
+	'survey:addpost' => "Create a survey",
+	'survey:editpost' => "Edit a survey: %s",
+	'survey:edit' => "Edit a survey",
+	'item:object:survey' => 'Surveys',
+	'item:object:survey_choice' => "Survey choices",
+	'survey:question' => "Survey question",
 	'survey:description' => "Description (optional)",
 	'survey:responses' => "Vote choices",
 	'survey:result:label' => "%s (%s)",
 	'survey:show_results' => "Show results",
-	'survey:show_poll' => "Show poll",
+	'survey:show_survey' => "Show survey",
 	'survey:add_choice' => "Add vote choice",
 	'survey:delete_choice' => "Delete this choice",
-	'survey:close_date' => "Poll closing date (optional)",
-	'survey:voting_ended' => "Voting for this poll ended on %s.",
-	'survey:poll_closing_date' => "(Poll closing date: %s)",
+	'survey:close_date' => "Survey closing date (optional)",
+	'survey:voting_ended' => "Voting for this survey ended on %s.",
+	'survey:survey_closing_date' => "(Survey closing date: %s)",
 
-	'survey:convert:description' => 'ATTENTION: there were %s existing polls found that still have the old vote choices data structure. These polls won\'t work correctly on this version of the poll plugin.',
-	'survey:convert' => 'Update existing polls now',
-	'survey:convert:confirm' => 'The update is irreversible. Are you sure you want to convert the poll vote choices data structure?',
+	'survey:convert:description' => 'ATTENTION: there were %s existing surveys found that still have the old vote choices data structure. These surveys won\'t work correctly on this version of the survey plugin.',
+	'survey:convert' => 'Update existing surveys now',
+	'survey:convert:confirm' => 'The update is irreversible. Are you sure you want to convert the survey vote choices data structure?',
 
-	'survey:settings:group:title' => "Allow group polls?",
-	'survey:settings:group_poll_default' => "yes, on by default",
-	'survey:settings:group_poll_not_default' => "yes, off by default",
+	'survey:settings:group:title' => "Allow group surveys?",
+	'survey:settings:group_survey_default' => "yes, on by default",
+	'survey:settings:group_survey_not_default' => "yes, off by default",
 	'survey:settings:no' => "no",
-	'survey:settings:group_access:title' => "If group polls are activated, who gets to create polls?",
+	'survey:settings:group_access:title' => "If group surveys are activated, who gets to create surveys?",
 	'survey:settings:group_access:admins' => "group owners and admins only",
 	'survey:settings:group_access:members' => "any group member",
-	'survey:settings:front_page:title' => "Admins can make a single poll at a time the site's \"Poll of the day\"? (Widget Manager plugin required for adding the corresponding widget to the index page)",
-	'survey:settings:allow_close_date:title' => "Allow setting a closing date for polls? (afterwards the results can still be viewed but voting is no longer permitted)",
-	'survey:settings:allow_open_poll:title' => "Allow open polls? (open polls show which user voted for which poll choice; if this option is enabled, admins can see who voted what on any polls)",
-	'survey:none' => "No polls found.",
-	'survey:not_found' => "The poll was not found.",
-	'survey:permission_error' => "You do not have permission to edit this poll.",
+	'survey:settings:front_page:title' => "Admins can make a single survey at a time the site's \"Survey of the day\"? (Widget Manager plugin required for adding the corresponding widget to the index page)",
+	'survey:settings:allow_close_date:title' => "Allow setting a closing date for surveys? (afterwards the results can still be viewed but voting is no longer permitted)",
+	'survey:settings:allow_open_survey:title' => "Allow open surveys? (open surveys show which user voted for which survey choice; if this option is enabled, admins can see who voted what on any surveys)",
+	'survey:none' => "No surveys found.",
+	'survey:not_found' => "The survey was not found.",
+	'survey:permission_error' => "You do not have permission to edit this survey.",
 	'survey:vote' => "Vote",
-	'survey:login' => "Please login if you would like to vote in this poll.",
-	'group:poll:empty' => "No polls",
-	'survey:settings:site_access:title' => "Who can create site-wide polls?",
+	'survey:login' => "Please login if you would like to vote in this survey.",
+	'group:survey:empty' => "No surveys",
+	'survey:settings:site_access:title' => "Who can create site-wide surveys?",
 	'survey:settings:site_access:admins' => "admins only",
 	'survey:settings:site_access:all' => "any logged-in user",
-	'survey:can_not_create' => "You do not have permission to create polls.",
-	'survey:front_page_label' => "Make this poll the site's new \"Poll of the day\"",
-	'survey:open_poll_label' => "Show in results which members voted for which choice (open poll)",
+	'survey:can_not_create' => "You do not have permission to create surveys.",
+	'survey:front_page_label' => "Make this survey the site's new \"Survey of the day\"",
+	'survey:open_survey_label' => "Show in results which members voted for which choice (open survey)",
 	'survey:show_voters' => "Show voters",
 
 	/**
-	 * Poll widget
+	 * Survey widget
 	 **/
-	'survey:latest_widget_title' => "Latest community polls",
-	'survey:latest_widget_description' => "Displays the most recent polls.",
-	'survey:latestgroup_widget_title' => "Latest group polls",
-	'survey:latestgroup_widget_description' => "Displays the most recent group polls.",
-	'survey:my_widget_title' => "My polls",
-	'survey:my_widget_description' => "This widget will display your polls.",
-	'survey:widget:label:displaynum' => "How many polls do you want to display?",
-	'survey:individual' => "Poll of the day",
-	'poll_individual:widget:description' => "Display the site's current \"Poll of the day\".",
-	'survey:widget:no_poll' => "There are no polls of %s yet.",
-	'survey:widget:nonefound' => "No polls found.",
+	'survey:latest_widget_title' => "Latest community surveys",
+	'survey:latest_widget_description' => "Displays the most recent surveys.",
+	'survey:latestgroup_widget_title' => "Latest group surveys",
+	'survey:latestgroup_widget_description' => "Displays the most recent group surveys.",
+	'survey:my_widget_title' => "My surveys",
+	'survey:my_widget_description' => "This widget will display your surveys.",
+	'survey:widget:label:displaynum' => "How many surveys do you want to display?",
+	'survey:individual' => "Survey of the day",
+	'survey_individual:widget:description' => "Display the site's current \"Survey of the day\".",
+	'survey:widget:no_survey' => "There are no surveys of %s yet.",
+	'survey:widget:nonefound' => "No surveys found.",
 	'survey:widget:think' => "Let %s know what you think!",
-	'survey:enable_poll' => "Enable polls",
+	'survey:enable_survey' => "Enable surveys",
 	'survey:noun_response' => "vote",
 	'survey:noun_responses' => "votes",
 	'survey:settings:yes' => "yes",
@@ -95,47 +95,47 @@ $en = array(
 	/**
 	 * Notifications
 	 **/
-	'survey:new' => 'A new poll',
-	'survey:notify:summary' => 'New poll called %s',
-	'survey:notify:subject' => 'New poll: %s',
+	'survey:new' => 'A new survey',
+	'survey:notify:summary' => 'New survey called %s',
+	'survey:notify:subject' => 'New survey: %s',
 	'survey:notify:body' =>
 '
-%s created a new poll:
+%s created a new survey:
 
 %s
 
-View and vote on the poll:
+View and vote on the survey:
 %s
 ',
 
 	/**
-	 * Poll river
+	 * Survey river
 	 **/
-	'survey:settings:create_in_river:title' => "Show poll creation in activity river?",
-	'survey:settings:vote_in_river:title' => "Show poll voting in activity river?",
-	'survey:settings:send_notification:title' => "Send notification when a poll is created? (Members will only receive notifications if their are friend with the creator of the poll or a member of the group the poll was added to. Additionally, notifications will only be sent to members who configured Elgg's notification settings accordingly)",
-	'river:create:object:poll' => '%s created a poll %s',
-	'river:vote:object:poll' => '%s voted on the poll %s',
-	'river:comment:object:poll' => '%s commented on the poll %s',
+	'survey:settings:create_in_river:title' => "Show survey creation in activity river?",
+	'survey:settings:vote_in_river:title' => "Show survey voting in activity river?",
+	'survey:settings:send_notification:title' => "Send notification when a survey is created? (Members will only receive notifications if their are friend with the creator of the survey or a member of the group the survey was added to. Additionally, notifications will only be sent to members who configured Elgg's notification settings accordingly)",
+	'river:create:object:survey' => '%s created a survey %s',
+	'river:vote:object:survey' => '%s voted on the survey %s',
+	'river:comment:object:survey' => '%s commented on the survey %s',
 
 	/**
 	 * Status messages
 	 */
-	'survey:added' => "Your poll was created.",
-	'survey:edited' => "Your poll was saved.",
+	'survey:added' => "Your survey was created.",
+	'survey:edited' => "Your survey was saved.",
 	'survey:responded' => "Thank you for responding, your vote was recorded.",
-	'survey:deleted' => "Your poll was successfully deleted.",
+	'survey:deleted' => "Your survey was successfully deleted.",
 	'survey:totalvotes' => "Total number of votes: %s",
-	'survey:voted' => "Your vote has been cast for this poll. Thank you for voting on this poll.",
+	'survey:voted' => "Your vote has been cast for this survey. Thank you for voting on this survey.",
 
 	/**
 	 * Error messages
 	 */
-	'survey:blank' => "Sorry: you need to fill in both the question and add at least one vote choice before you can create the poll.",
-	'survey:novote' => "Sorry: you need to choose an option to vote in this poll.",
+	'survey:blank' => "Sorry: you need to fill in both the question and add at least one vote choice before you can create the survey.",
+	'survey:novote' => "Sorry: you need to choose an option to vote in this survey.",
 	'survey:alreadyvoted' => "Sorry: you can vote only once.",
-	'survey:notfound' => "Sorry: we could not find the specified poll.",
-	'survey:notdeleted' => "Sorry: we could not delete this poll.",
+	'survey:notfound' => "Sorry: we could not find the specified survey.",
+	'survey:notdeleted' => "Sorry: we could not delete this survey.",
 	
 	
 	
