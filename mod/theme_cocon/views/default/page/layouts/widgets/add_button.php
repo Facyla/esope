25949f60675a097d4f037de0c57b0a64e1388c9a
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-03-13 18:27:00 +0100

    Widget style

--- mod/theme_cocon/views/default/page/layouts/widgets/add_button.php
+++ mod/theme_cocon/views/default/page/layouts/widgets/add_button.php
@@ -9,10 +9,10 @@ $picto_url = $CONFIG->url . 'mod/theme_cocon/graphics/pictos/';
 	<div class="cocon-widget-add-control">
 		<?php
 		//echo '<img src="' . $picto_url . 'widgets_edit.png" />&nbsp;';
-		echo '<i class="fa fa-pencil"></i>&nbsp;';
+		//echo '<i class="fa fa-pencil"></i>&nbsp;';
 		echo elgg_view('output/url', array(
 			'href' => '#widgets-add-panel',
-			'text' => elgg_echo('widgets:add'),
+			'text' => '<i class="fa fa-pencil"></i>&nbsp;' . elgg_echo('widgets:add'),
 			'class' => 'cocon-widget-add-button',
 			'rel' => 'toggle',
 		));
