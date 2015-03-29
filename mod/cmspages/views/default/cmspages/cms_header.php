824cda4fb7ace3478fe05345d59cb688a01401f8
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-03-29 23:18:23 +0200

    Add custom header above menu

--- mod/cmspages/views/default/cmspages/cms_header.php
+++ mod/cmspages/views/default/cmspages/cms_header.php
@@ -2,6 +2,8 @@
 // Init custom CMS menu based on categories
 cmspages_set_categories_menu();
 
+echo elgg_view('cmspages/view', array('pagetype' => 'cms-header'));
+
 echo '<div id="transverse"><div class="interne"><nav>';
 echo elgg_view_menu('cmspages_categories', array('class' => 'elgg-menu-hz menu-navigation', 'sort_by' => 'weight'));
 echo '</nav></div></div>';
