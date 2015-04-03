241be74fb8f29b5d8444fd69068fc56e9d007143
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-04-03 22:37:56 +0200

    Cleaner

--- mod/plugin_template/views/default/plugins/plugin_template/usersettings.php
+++ mod/plugin_template/views/default/plugins/plugin_template/usersettings.php
@@ -8,14 +8,14 @@ $yes_no_opt = array( 'yes' => elgg_echo('option:yes'), 'no' => elgg_echo('option
 
 
 // Set default value
-if (!isset($vars['entity']->user_plugin_setting)) { $vars['entity']->user_plugin_setting == 'default'; }
+//if (!isset($vars['entity']->settingname)) { $vars['entity']->settingname == 'default'; }
 
 
 // Example yes/no setting
-echo '<p><label>Test select setting "user_plugin_setting"</label> ' . elgg_view('input/dropdown', array('name' => 'params[user_plugin_setting]', 'options_values' => $yes_no_opt, 'value' => $vars['entity']->user_plugin_setting)) . '</p>';
+//echo '<p><label>' . elgg_echo('plugin_template:settings:settingname'). ' ' . elgg_view('input/dropdown', array('name' => 'params[settingname]', 'options_values' => $yes_no_opt, 'value' => $vars['entity']->settingname)) . '</label><br /><em>' . elgg_echo('plugin_template:settings:settingname:details'). '</em></p>';
 
 
 // Example text setting
-echo '<p><label>Text setting "user_plugin_setting2"</label> ' . elgg_view('input/dropdown', array('name' => 'params[user_plugin_setting2]', 'value' => $vars['entity']->user_plugin_setting2)) . '</p>';
+//echo '<p><label>' . elgg_echo('plugin_template:settings:settingname'). ' ' . elgg_view('input/dropdown', array('name' => 'params[settingname]', 'value' => $vars['entity']->settingname)) . '</label><br /><em>' . elgg_echo('plugin_template:settings:settingname:details'). '</em></p>';
 
 
