64c288a8380576c0e785a0d59f633521bcc92aa6
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-04-05 00:47:26 +0200

    route hook

--- mod/download_counter/start.php
+++ mod/download_counter/start.php
@@ -22,7 +22,7 @@ function download_counter_init() {
 	
 	// @TODO plugin hook on page handler, as there is not necessarly an action...
 	// Route => return not false pour poursuivre : $request = array('handler' => $handler, 'segments' => $page);
-	// elgg_register_plugin_hook('route', 'download_counter_route')
+	// elgg_register_plugin_hook('route', 'all', 'download_counter_route');
 	
 	// Get a plugin setting
 	//$setting = elgg_get_plugin_setting('setting_name', 'download_counter');
@@ -43,5 +43,49 @@ function download_counter_function() {
 }
 */
 
+/* Filtrage via le page_handler
+ * 
+ * Principes de conception : 
+ * - ne filtre que si on l'a explicitement demandé quelque part (pas de modification du comportement par défaut
+ * 
+*/
+function download_counter_route($hook, $type, $return, $params) {
+	/*
+	global $CONFIG;
+	$home = $CONFIG->url;
+	
+	// Page handler et segments de l'URL
+	// Note : les segments commencent après le page_handler (ex.: URL: groups/all donne 0 => 'all')
+	$handler = $return['handler'];
+	$segments = $return['segments'];
+	//echo print_r($segments, true); // debug
+	//register_error($handler . ' => ' . print_r($segments, true));
+	//error_log('DEBUG externalmembers ROUTE : ' . $handler . ' => ' . print_r($segments, true));
+	
+	if (!elgg_is_logged_in()) {
+		// Il n'y a verrouillage du profil que si cette option est explicitement activée (pour ne pas modifier le comportement par défaut)
+		$public_profiles = elgg_get_plugin_setting('public_profiles', 'adf_public_platform');
+		if ($public_profiles == 'yes') {
+			if ($handler == 'profile') {
+				$username = $segments[0];
+				if ($user = get_user_by_username($username)) {
+					esope_user_profile_gatekeeper($user);
+				}
+			}
+		}
+	}
+	
+	//	@todo : Pour tous les autres cas => déterminer le handler et ajuster le comportement
+	//register_error("L'accès à ces pages n'est pas encore déterminé : " . $handler . ' / ' . print_r($segments, true));
+	//error_log("L'accès à ces pages n'est pas encore déterminé : " . $handler . ' / ' . print_r($segments, true));
+	
+	/* Valeurs de retour :
+	 * return false; // Interrompt la gestion des handlers
+	 * return $params; // Laisse le fonctionnement habituel se poursuivre
+	*/
+	// Par défaut on ne fait rien du tout
+	return $params;
+	*/
+}
 
 
