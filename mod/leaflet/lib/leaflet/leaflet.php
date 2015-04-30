ccd165a1b76156c8758cde62aee0cf4dd485c305
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-04-30 15:44:52 +0200

    Batch geocoding cron

--- mod/leaflet/lib/leaflet/leaflet.php
+++ mod/leaflet/lib/leaflet/leaflet.php
@@ -44,53 +44,53 @@ function leaflet_batch_add_member_marker($user, $getter, $options) {
 	$forceupdate = false;
 	
 	// Geocode only members locations that are set...
-	if (empty(trim($location))) return;
-	
-	/* Retro-convert tags to text content, if location was previously set as tags...
-	 * If location field was set to "tags", please change it first to "text"
-	 * Then move next line out of comment block and display the map once to convert, then move it back here
-	$location = implode(', ', $user->location); $user->location = $location;
-	*/
-	
-	// Get coordinates
-	$lat = $user->getLatitude();
-	$long = $user->getLongitude();
-	
-	
-	// Add geolocation update check (using previous valid location)
-	if (!(empty($user->prev_location) && ($location != $user->prev_location))) { $forceupdate = true; }
-	
-	// Geocoding, if needed or required
-	if (!$lat || !$long || $forceupdate) {
-		// Remember latest geocoded location (to avoid geocoding it again if it didn't change
-		// ..whether it succeeded or failed it would be useless to do it again
-		$user->prev_location = $location;
+	if (!empty(trim($location))) {
+		/* Retro-convert tags to text content, if location was previously set as tags...
+		 * If location field was set to "tags", please change it first to "text"
+		 * Then move next line out of comment block and display the map once to convert, then move it back here
+		$location = implode(', ', $user->location); $user->location = $location;
+		*/
 		
-		//error_log("Geocoding location... for $user->username : $user->location"); // debug
-		$geo_location = elgg_geocode_location($location);
-		if ($geo_location) {
-			$lat = (float)$geo_location['lat'];
-			$long = (float)$geo_location['long'];
-			// Save successfully geocoded location
-			if ($lat && $long) { $user->setLatLong($lat, $long); }
+		// Get coordinates
+		$lat = $user->getLatitude();
+		$long = $user->getLongitude();
+		
+		// Add geolocation update check (using previous valid location)
+		if (!(empty($user->prev_location) && ($location != $user->prev_location))) { $forceupdate = true; }
+		
+		// Geocoding, if needed or required
+		if (!$lat || !$long || $forceupdate) {
+			// Remember latest geocoded location (to avoid geocoding it again if it didn't change
+			// ..whether it succeeded or failed it would be useless to do it again
+			$user->prev_location = $location;
+			
+			//error_log("Geocoding location... for $user->username : $user->location"); // debug
+			$geo_location = elgg_geocode_location($location);
+			if ($geo_location) {
+				$lat = (float)$geo_location['lat'];
+				$long = (float)$geo_location['long'];
+				// Save successfully geocoded location
+				if ($lat && $long) { $user->setLatLong($lat, $long); }
+			}
 		}
-	}
-	
-	
-	// Render JS marker code
-	if ($lat && $long) {
-		$name = $user->name;
-		$name = json_encode($name);
 		
-		$description = elgg_view_entity($user, array('full_view' => false));
-		$description = json_encode($description);
 		
-		echo "
-			marker = L.marker([$lat, $long], {icon: onlineUsersMarker, title: $name});
-			marker.bindPopup($description);
-			onlineUsersMarkers.addLayer(marker);
-			bounds.extend(marker.getLatLng());
-			";
+		// Render JS marker code
+		if ($lat && $long) {
+			$name = $user->name;
+			$name = json_encode($name);
+			
+			$description = elgg_view_entity($user, array('full_view' => false));
+			$description = json_encode($description);
+			
+			echo "
+				marker = L.marker([$lat, $long], {icon: onlineUsersMarker, title: $name});
+				marker.bindPopup($description);
+				onlineUsersMarkers.addLayer(marker);
+				bounds.extend(marker.getLatLng());
+				";
+		}
+		
 	}
 }
 
