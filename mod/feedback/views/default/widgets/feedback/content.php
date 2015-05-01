4b7ca1a9de59ebed47b528bfc4dd4c5d07c91297
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-05-01 20:03:32 +0200

    Open feedback only in admin widget

--- mod/feedback/views/default/widgets/feedback/content.php
+++ mod/feedback/views/default/widgets/feedback/content.php
@@ -9,21 +9,23 @@
  * List the latest feedback entries
  */
 
+$num = $vars['entity']->num_display;
+
 elgg_push_context('widget');
 
 switch($vars['entity']->status) {
 	case 'open':
-		$list = elgg_list_entities_from_metadata(array('types' => 'object', 'subtypes' => 'feedback', 'metadata_name_value_pairs' => array('name' => 'status', 'value' => 'closed', 'operand' => '!='), 'limit' => $vars['entity']->num_display, 'pagination' => false));
+		$list = elgg_list_entities_from_metadata(array('types' => 'object', 'subtypes' => 'feedback', 'metadata_name_value_pairs' => array('name' => 'status', 'value' => 'open'), 'limit' => $num, 'pagination' => false));
 		if (!$list) { $list = '<p class="mtm">' . elgg_echo('feedback:list:noopenfeedback') . '</p>'; }
 		break;
 	
 	case 'closed':
-		$list = elgg_list_entities_from_metadata(array('types' => 'object', 'subtypes' => 'feedback', 'metadata_name_value_pairs' => array('name' => 'status', 'value' => 'closed'), 'limit' => $vars['entity']->num_display, 'pagination' => false));
+		$list = elgg_list_entities_from_metadata(array('types' => 'object', 'subtypes' => 'feedback', 'metadata_name_value_pairs' => array('name' => 'status', 'value' => 'closed'), 'limit' => $num, 'pagination' => false));
 		if (!$list) { $list = '<p class="mtm">' . elgg_echo('feedback:list:nofeedback') . '</p>'; }
 		break;
 	
 	default:
-		$list = elgg_list_entities(array('types' => 'object', 'subtypes' => 'feedback', 'limit' => $vars['entity']->num_display, 'pagination' => false));
+		$list = elgg_list_entities(array('types' => 'object', 'subtypes' => 'feedback', 'limit' => $num, 'pagination' => false));
 		if (!$list) { $list = '<p class="mtm">' . elgg_echo('feedback:list:nofeedback') . '</p>'; }
 }
 
