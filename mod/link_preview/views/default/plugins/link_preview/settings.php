77eafdfeb3a1abf5e534f319d1761b70956284f7
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-05-19 18:06:44 +0200

    Link preview

--- mod/link_preview/views/default/plugins/link_preview/settings.php
+++ mod/link_preview/views/default/plugins/link_preview/settings.php
@@ -2,18 +2,41 @@
 $url = elgg_get_site_url();
 
 // Define dropdown options
-$yes_no_opt = array( 'yes' => elgg_echo('option:yes'), 'no' => elgg_echo('option:no') );
+$yes_no_opt = array('yes' => elgg_echo('option:yes'), 'no' => elgg_echo('option:no'));
 
+$trigger_opt = array('hover' => elgg_echo('hover'), 'click' => elgg_echo('click'));
+$scale_opt = array('1' => "1 (original)", '0.75' => "0.75", '0.5' => "0.5", '0.3' => "0.3 (default)", '0.2' => "0.2", '0.1' => "0.1");
+$position_opt = array('right' => elgg_echo('right'), 'left' => elgg_echo('left'));
 
 // Set default value
-//if (!isset($vars['entity']->setting_name)) { $vars['entity']->setting_name = 'default'; }
+if (!isset($vars['entity']->trigger)) { $vars['entity']->trigger = 'hover'; }
+if (!isset($vars['entity']->scale)) { $vars['entity']->scale = '0.3'; }
+if (!isset($vars['entity']->targetwidth)) { $vars['entity']->targetwidth = '1200'; }
+if (!isset($vars['entity']->targetheight)) { $vars['entity']->targetheight = '800'; }
+if (!isset($vars['entity']->offset)) { $vars['entity']->offset = '50'; }
+if (!isset($vars['entity']->position)) { $vars['entity']->position = 'right'; }
 
 
-// Example yes/no setting
-//echo '<p><label>' . elgg_echo('plugin_template:settings:settingname'). ' ' . elgg_view('input/dropdown', array('name' => 'params[settingname]', 'options_values' => $yes_no_opt, 'value' => $vars['entity']->settingname)) . '</label><br /><em>' . elgg_echo('plugin_template:settings:settingname:details'). '</em></p>';
+
+// Preview mode : hover / click
+echo '<p><label>' . elgg_echo('link_preview:setting:trigger'). ' ' . elgg_view('input/dropdown', array('name' => 'params[trigger]', 'options_values' => $trigger_opt, 'value' => $vars['entity']->trigger)) . '</label><br /><em>' . elgg_echo('link_preview:setting:trigger:details'). '</em></p>';
+
+// Scale ratio
+echo '<p><label>' . elgg_echo('link_preview:setting:scale'). ' ' . elgg_view('input/dropdown', array('name' => 'params[scale]', 'options_values' => $scale_opt, 'value' => $vars['entity']->scale)) . '</label><br /><em>' . elgg_echo('link_preview:setting:scale:details'). '</em></p>';
+
+// Target height
+echo '<p><label>' . elgg_echo('link_preview:setting:targetheight'). ' ' . elgg_view('input/text', array('name' => 'params[targetheight]', 'value' => $vars['entity']->targetheight)) . '</label><br /><em>' . elgg_echo('link_preview:setting:targetheight:details'). '</em></p>';
+
+// Target width
+echo '<p><label>' . elgg_echo('link_preview:setting:targetwidth'). ' ' . elgg_view('input/text', array('name' => 'params[targetwidth]', 'value' => $vars['entity']->targetwidth)) . '</label><br /><em>' . elgg_echo('link_preview:setting:targetwidth:details'). '</em></p>';
+
+// Offset
+echo '<p><label>' . elgg_echo('link_preview:setting:offset'). ' ' . elgg_view('input/text', array('name' => 'params[offset]', 'value' => $vars['entity']->offset)) . '</label><br /><em>' . elgg_echo('link_preview:setting:offset:details'). '</em></p>';
+
+// Position
+echo '<p><label>' . elgg_echo('link_preview:setting:position'). ' ' . elgg_view('input/dropdown', array('name' => 'params[position]', 'value' => $vars['entity']->position, 'options_values' => $position_opt)) . '</label><br /><em>' . elgg_echo('link_preview:setting:position:details'). '</em></p>';
 
 
-// Example text setting
-//echo '<p><label>' . elgg_echo('plugin_template:settings:settingname'). ' ' . elgg_view('input/text', array('name' => 'params[setting_name2]', 'value' => $vars['entity']->setting_name2)) . '</label><br /><em>' . elgg_echo('plugin_template:settings:settingname:details'). '</em></p>';
+// @TODO : allow to set selectors on which to apply livePreview
 
 
