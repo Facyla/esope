386411a2c202ee571834733b303a990e4db6fc0b
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-05-21 16:07:51 +0200

    FR translation complete

--- mod/poll/languages/fr.php
+++ mod/poll/languages/fr.php
@@ -28,48 +28,48 @@ $fr = array(
 	'poll:voting_ended' => "Les votes pour ce sondage se terminent le %s.",
 	'poll:poll_closing_date' => "(Date de clôture du sondage : %s)",
 
-	'poll:convert:description' => 'ATTENTION: there were %s existing polls found that still have the old vote choices data structure. These polls won\'t work correctly on this version of the poll plugin.',
+	'poll:convert:description' => "ATTENTION : %s sondages existant utilisent encore l'ancienne structure de données pour les choix des votes. Ces sondages ne vont plus fonctionner avec cette version du plugin \"poll\".",
 	'poll:convert' => 'Mettre à jour les sondages maintenant',
-	'poll:convert:confirm' => 'The update is irreversible. Are you sure you want to convert the poll vote choices data structure?',
+	'poll:convert:confirm' => "La mise à jour est irréversible. Confirmez-vous vouloir convertir la structure de données des choix des votes ?",
 
 	'poll:settings:group:title' => "Activer les sondages du groupe ?",
-	'poll:settings:group_poll_default' => "oui, activé par défaut",
-	'poll:settings:group_poll_not_default' => "oui, désactivé par défaut",
-	'poll:settings:no' => "non",
-	'poll:settings:group_access:title' => "If group polls are activated, who gets to create polls?",
-	'poll:settings:group_access:admins' => "responsables du groupe et administrayeurs seulement",
-	'poll:settings:group_access:members' => "tout membre du groupe",
-	'poll:settings:front_page:title' => "Admins can make a single poll at a time the site's \"Poll of the day\"? (Widget Manager plugin required for adding the corresponding widget to the index page)",
-	'poll:settings:allow_close_date:title' => "Allow setting a closing date for polls? (afterwards the results can still be viewed but voting is no longer permitted)",
-	'poll:settings:allow_open_poll:title' => "Allow open polls? (open polls show which user voted for which poll choice; if this option is enabled, admins can see who voted what on any polls)",
+	'poll:settings:group_poll_default' => "Oui, activé par défaut",
+	'poll:settings:group_poll_not_default' => "Oui, désactivé par défaut",
+	'poll:settings:no' => "Non",
+	'poll:settings:group_access:title' => "Si les sondages de groupe sont activés, qui peut créer des sondages ?",
+	'poll:settings:group_access:admins' => "responsables du groupe et administrateurs seulement",
+	'poll:settings:group_access:members' => "tout les membres du groupe",
+	'poll:settings:front_page:title' => "Les administrateurs peuvent-ils définir un sondage à la fois comme \"Sondage du jour\" ? (le plugin Widget Manager est requis pour ajouter le widget correspondant sur la page d'accueil)",
+	'poll:settings:allow_close_date:title' => "Permettre de définir une date de fin pour les sondages ? (après cette date les résultats seront toujours visibles, mais les votes ne seront plus permis)",
+	'poll:settings:allow_open_poll:title' => "Activer les sondages ouverts ? (les sondages ouverts affichent les noms des membres qui ont voté pour chacun des choix du sondage ; si cette option est activée, les administrateurs peuvent voir qui a voté pour quel choix sur tous les sondages)",
 	'poll:none' => "Aucun sondage.",
-	'poll:permission_error' => "You do not have permission to edit this poll.",
+	'poll:permission_error' => "Vous n'avez pas la permission de modifier ce sondage.",
 	'poll:vote' => "Voter",
-	'poll:login' => "Please login if you would like to vote in this poll.",
+	'poll:login' => "Veuillez vous connecter pour voter sur ce sondage.",
 	'group:poll:empty' => "Aucun sondage",
 	'poll:settings:site_access:title' => "Qui peut créer des sondages pour l'ensemble du site ?",
 	'poll:settings:site_access:admins' => "administrateurs seulement",
-	'poll:settings:site_access:all' => "tout membre du site",
-	'poll:can_not_create' => "You do not have permission to create polls.",
-	'poll:front_page_label' => "Make this poll the site's new \"Poll of the day\"",
-	'poll:open_poll_label' => "Show in results which members voted for which choice (open poll)",
-	'poll:show_voters' => "Montrer les votes",
+	'poll:settings:site_access:all' => "tous les membres du site",
+	'poll:can_not_create' => "Vous n'avez pas la permission de créer des sondages.",
+	'poll:front_page_label' => "Faire de ce sondage le nouveau \"Sondage du jour\"",
+	'poll:open_poll_label' => "Afficher dans les résultats quels membres ont voté pour quel choix (sondage ouvert)",
+	'poll:show_voters' => "Afficher les votes",
 
 	/**
 	 * Poll widget
 	 **/
-	'poll:latest_widget_title' => "Latest community polls",
-	'poll:latest_widget_description' => "Displays the most recent polls.",
+	'poll:latest_widget_title' => "Sondages récents",
+	'poll:latest_widget_description' => "Affiche les sondages les plus récents.",
 	'poll:latestgroup_widget_title' => "Derniers sondages du groupe",
 	'poll:latestgroup_widget_description' => "Affiche les derniers sondages du groupe.",
 	'poll:my_widget_title' => "Mes sondages",
-	'poll:my_widget_description' => "This widget will display your polls.",
-	'poll:widget:label:displaynum' => "How many polls do you want to display?",
+	'poll:my_widget_description' => "Ce widget affiche vos propres sondages.",
+	'poll:widget:label:displaynum' => "Combien de sondages afficher ?",
 	'poll:individual' => "Sondage du jour",
-	'poll_individual:widget:description' => "Display the site's current \"Poll of the day\".",
-	'poll:widget:no_poll' => "There are no polls of %s yet.",
+	'poll_individual:widget:description' => "Affiche le \"Sondage du jour\" du site.",
+	'poll:widget:no_poll' => "Il n'y a aucun sondage pour %s pour le moment.",
 	'poll:widget:nonefound' => "Aucun sondage.",
-	'poll:widget:think' => "Let %s know what you think!",
+	'poll:widget:think' => "Dites à %s ce que vous en pensez !",
 	'poll:enable_poll' => "Activer les sondages",
 	'poll:noun_response' => "vote",
 	'poll:noun_responses' => "votes",
@@ -93,7 +93,7 @@ $fr = array(
 	 * Notifications
 	 **/
 	'poll:new' => 'Un nouveau sondage',
-	'poll:notify:summary' => 'New poll called %s',
+	'poll:notify:summary' => "Nouveau sondage intitulé %s",
 	'poll:notify:subject' => 'Nouveau sondage : %s',
 	'poll:notify:body' =>'
 %s a créé un nouveau sondage :
@@ -107,30 +107,30 @@ Afficher le sondage et voter :
 	/**
 	 * Poll river
 	 **/
-	'poll:settings:create_in_river:title' => "Show poll creation in activity river?",
-	'poll:settings:vote_in_river:title' => "Show poll voting in activity river?",
-	'poll:settings:send_notification:title' => "Send notification when a poll is created? (Members will only receive notifications if their are friend with the creator of the poll or a member of the group the poll was added to. Additionally, notifications will only be sent to members who configured Elgg's notification settings accordingly)",
-	'river:create:object:poll' => '%s created a poll %s',
-	'river:vote:object:poll' => '%s voted on the poll %s',
-	'river:comment:object:poll' => '%s commented on the poll %s',
+	'poll:settings:create_in_river:title' => "Afficher les sondages dans le fil d'activité ?",
+	'poll:settings:vote_in_river:title' => "Afficher les votes sur les sondages dans le fil d'activité ?",
+	'poll:settings:send_notification:title' => "Envoyer une notification lorsqu'un nouveau sondage est créé ? (les membres ne recevront de notification que s'ils sont en contact avec l'auteur du sondage, ou membre du groupe où le sondage a été publié. De plus, les notifications ne seront envoyées qu'aux membres dont les paramètres de notification permettent l'envoi de notifications)",
+	'river:create:object:poll' => '%s a créé le sondage %s',
+	'river:vote:object:poll' => '%s a voté sur le sondage %s',
+	'river:comment:object:poll' => '%s a commenté le sondage %s',
 
 	/**
 	 * Status messages
 	 */
-	'poll:added' => "Your poll was created.",
-	'poll:edited' => "Your poll was saved.",
-	'poll:responded' => "Thank you for responding, your vote was recorded.",
-	'poll:deleted' => "Your poll was successfully deleted.",
-	'poll:totalvotes' => "Total number of votes: ",
-	'poll:voted' => "Your vote has been cast for this poll. Thank you for voting on this poll.",
+	'poll:added' => "Votre sondage a bien été créé.",
+	'poll:edited' => "Votre sondage a bien été enregistré.",
+	'poll:responded' => "Merci d'avoir répondu, votre vote a bien été enregistré.",
+	'poll:deleted' => "Votre sondage a bien été supprimé.",
+	'poll:totalvotes' => "Nombre total de votes : ",
+	'poll:voted' => "Votre vote a bien été enregistré. Merci d'avoir voté sur ce sondage.",
 
 	/**
 	 * Error messages
 	 */
-	'poll:blank' => "Désolé : you need to fill in both the question and add at least one vote choice before you can create the poll.",
-	'poll:novote' => "Désolé : you need to choose an option to vote in this poll.",
-	'poll:notfound' => "Désolé : we could not find the specified poll.",
-	'poll:notdeleted' => "Désolé : we could not delete this poll."
+	'poll:blank' => "Désolé : vous devez compléter à la fois la question, et ajouter au moins un choix pour le vote avant de pouvoir enregistrer votre sondage.",
+	'poll:novote' => "Désolé : vous devez choisir une option pour voter dans ce sondage.",
+	'poll:notfound' => "Désolé : impossible de trouver le sondage correspondant.",
+	'poll:notdeleted' => "Désolé : impossible de supprimer ce sondage."
 );
 
 add_translation("fr",$fr);
