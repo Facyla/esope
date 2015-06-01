ccb299896c1bd852c65814006022ee57e0df54f1
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-06-01 18:14:52 +0200

    Inscription réseau Fing

--- mod/theme_fing/views/default/theme_fing/css.php
+++ mod/theme_fing/views/default/theme_fing/css.php
@@ -96,8 +96,8 @@ header h1 a:hover, header h1 a:focus, header h1 a:active { color: #000; text-dec
 
 .profile_manager_register_category { padding-top: 2ex; }
 .elgg-form-register .captcha { padding-top: 2ex; }
-.elgg-form-register #profile_manager_register_left { width:55%; }
-.elgg-form-register #profile_manager_register_right { width:41%; }
+.elgg-form-register #profile_manager_register_left { width:50%; }
+.elgg-form-register #profile_manager_register_right { width:44%; }
 @media (max-width:700px) {
 	#profile_manager_register_left, #profile_manager_register_right { width:100%; float:none; }
 }
