e4db388be2c6b0be31a2e44a2b7d68968d7f76d0
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-06-03 15:22:36 +0200

    Registration details

--- mod/theme_fing/views/default/input/captcha.php
+++ mod/theme_fing/views/default/input/captcha.php
@@ -1,13 +1,12 @@
 <?php
+// Generate a token which is then passed into the captcha algorithm for verification
 
-	// Generate a token which is then passed into the captcha algorithm for verification
-
-	$challenge = vazco_text_captcha::getCaptchaChallenge();
-	list($challenge_id, $challenge_question, $challenge_answer) = $challenge;  
+$challenge = vazco_text_captcha::getCaptchaChallenge();
+list($challenge_id, $challenge_question, $challenge_answer) = $challenge;  
 ?>
 <div class="fing-register-captcha mandatory">
 	<input type="hidden" name="captcha_token" value="<?php echo $challenge_id; ?>" />
-	<label><?php echo $challenge_question; ?></label>
+	<label><?php echo $challenge_question; ?><br /><small><?php echo elgg_echo('theme_fing:register:antispam'); ?></small></label>
 	
 	<div class="profile_manager_register_input_container">
 		<?php echo elgg_view('input/text', array('name' => 'captcha_input', 'class' => 'captcha-input-text')); ?>
