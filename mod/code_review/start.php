0947c2cd24f42072fc41049426ee7e7d7edc1eb9
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-06-09 19:06:06 +0200

    Code review, useful for upgrades

---
+++ mod/code_review/start.php
@@ -0,0 +1,3 @@
+<?php
+code_review::boot();
+elgg_register_event_handler('init', 'system', array('code_review', 'init'));
---
+++ mod/code_review/views/default/admin/code/diagnostic.php
@@ -0,0 +1,32 @@
+<?php
+
+$language = get_input('language');
+
+$body = elgg_view_form('code_review/select', array(
+	'action' => '#',
+), array(
+	'language' => $language,
+));
+
+echo elgg_view_module('main', elgg_echo('code_review:form'), $body);
+
+echo '<br>';
+
+$body = '';
+$body .= elgg_view('graphics/ajax_loader', array(
+	'id' => 'code-review-loader'
+));
+$body .= '<div id="code-review-result">';
+
+if ($language) {
+	$body .= elgg_view('code_review/analysis', array(
+		'language' => $language,
+	));
+} else {
+	$body .= elgg_echo('code_review:results:initial_stub');
+}
+
+$body .= '</div>';
+
+echo elgg_view_module('main', elgg_echo('code_review:results'), $body);
+
