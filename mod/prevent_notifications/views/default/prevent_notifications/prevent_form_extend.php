6f249d01a838a1d9a71e51723240ecb33f7b5fbb
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-06-25 14:50:53 +0200

    Defaut setting

--- mod/prevent_notifications/views/default/prevent_notifications/prevent_form_extend.php
+++ mod/prevent_notifications/views/default/prevent_notifications/prevent_form_extend.php
@@ -5,6 +5,6 @@ $value = elgg_extract('value', $vars, $default);
 <div id="prevent_notification">
 	<img src="<?php echo $vars['url']; ?>mod/prevent_notifications/graphics/notify.png" />
 	<label><?php echo elgg_echo('prevent_notifications:label'); ?></label><br />
-	<?php echo elgg_view("input/radio", array("name" => 'send_notification', "value" => $value, 'options' => array(elgg_echo('prevent_notifications:yes') => 'yes', elgg_echo('prevent_notifications:no') => 'no') )); ?>
+	<?php echo elgg_view("input/radio", array("name" => 'send_notification', "value" => $value, 'options' => array(elgg_echo('prevent_notifications:yes') => 'yes', elgg_echo('prevent_notifications:no') => 'no'), 'required' => "required")); ?>
 </div>
 
