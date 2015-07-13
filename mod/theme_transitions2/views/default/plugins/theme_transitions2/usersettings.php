edfb89665d53781e3678fb964781fc444cb3c199
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-07-13 18:26:42 +0200

    Menus

--- mod/theme_transitions2/views/default/plugins/theme_transitions2/usersettings.php
+++ mod/theme_transitions2/views/default/plugins/theme_transitions2/usersettings.php
@@ -1,6 +1,7 @@
 <?php
 
 // Define dropdown options
+$yes_no_opt = array('yes' => elgg_echo('option:yes'), 'no' => elgg_echo('option:no'));
 $no_yes_opt = array('no' => elgg_echo('option:no'), 'yes' => elgg_echo('option:yes'));
 
 echo '<p><label>' . elgg_echo('theme_transitions2:usersettings:public_profile') . ' ' . elgg_view('input/dropdown', array('name' => 'params[public_profile]', 'options_values' => $no_yes_opt, 'value' => $vars['entity']->public_profile)) . '</label></p>';
