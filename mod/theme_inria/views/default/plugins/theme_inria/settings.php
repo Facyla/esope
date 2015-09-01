89af0cf036aaa866ef9061f3cde021a668844840
Florian DANIEL (Facyla) <facyla@gmail.com>
2015-09-01 16:26:45 +0200

    Home slider

--- mod/theme_inria/views/default/plugins/theme_inria/settings.php
+++ mod/theme_inria/views/default/plugins/theme_inria/settings.php
@@ -77,7 +77,7 @@ echo "<fieldset><legend>Activation du cron d'actualisation des données LDAP</le
 echo '</fieldset>';
 
 
-echo "<p><label>GUID ou identifiant du slider à afficher sur l'accueil " . elgg_view('input/text', array('name' => 'home_slider', 'value' => $vars['entity']->home_slider)) . '</label></p>';
+echo "<p><label>GUID ou identifiant du slider à afficher sur l'accueil " . elgg_view('input/text', array('name' => 'params[home_slider]', 'value' => $vars['entity']->home_slider)) . '</label></p>';
 
 
 
