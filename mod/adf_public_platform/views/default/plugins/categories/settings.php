7c5a078c7fd40f4f97beabb99d1f8f607327edba
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-09-24 22:31:03 +0200

    categories

---
+++ mod/adf_public_platform/views/default/plugins/categories/settings.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Administrator sets the categories for the site
+ *
+ * @package ElggCategories
+ */
+
+// Get site categories
+$site = elgg_get_site_entity();
+$categories = $site->categories;
+
+//if (empty($categories)) { $categories = array(); }
+if (empty($categories)) { $categories = ''; }
+$categories = implode(",\n", $categories);
+
+?>
+<div>
+	<p><?php echo elgg_echo('categories:explanation'); ?></p>
+	<p><?php echo elgg_echo('esope:categories:tree:explanation'); ?></p>
+	<?php
+	//echo elgg_view('input/tags', array('value' => $categories, 'name' => 'categories'));
+	echo elgg_view('input/plaintext', array('value' => $categories, 'name' => 'categories'));
+	// @TODO : automatically add commas before newlines, smthg like : yourString.split(",").join("\n")
+	?>
+</div>
