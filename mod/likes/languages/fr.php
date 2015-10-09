0c6d8aac65ae2cb1d18020ad38b3b2a8f91765f8
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-10-09 12:59:12 +0200

    Full replaced translation

--- mod/likes/languages/fr.php
+++ mod/likes/languages/fr.php
@@ -1,30 +1,29 @@
 <?php
 return array(
-	'likes:this' => 'a aimé çà',
-	'likes:deleted' => 'Votre appréciation a été retiré',
-	'likes:see' => 'Voir ceux qui ont aimé çà',
-	'likes:remove' => 'N\'aime pas çà',
+	'likes:this' => 'apprécie',
+	'likes:deleted' => 'Votre appréciation a été retirée',
+	'likes:see' => 'Voir ceux qui apprécient',
+	'likes:remove' => 'N\'apprécie plus',
 	'likes:notdeleted' => 'Il y a eu un problème, lors de la suppresion d\'appréciation',
-	'likes:likes' => 'Vous aimez çà maintenant',
+	'likes:likes' => 'Vous appréciez',
 	'likes:failure' => 'Il y a eu un problème d\'appréciation sur cet élément',
-	'likes:alreadyliked' => 'Vous avez déjà porté votre appréciation çà',
+	'likes:alreadyliked' => 'Vous avez déjà porté votre appréciation',
 	'likes:notfound' => 'L\'élément que vous essayez d\'apprécier ne peut être trouvé',
-	'likes:likethis' => 'Aime çà',
-	'likes:userlikedthis' => '%s aime',
-	'likes:userslikedthis' => '%s aiment',
-	'likes:river:annotate' => 'aime',
-	'likes:delete:confirm' => 'Etes-vous sûr que vous voulez effacer cette appréciation ?',
+	'likes:likethis' => 'Apprécie',
+	'likes:userlikedthis' => '%s apprécie',
+	'likes:userslikedthis' => '%s apprécient',
+	'likes:river:annotate' => 'apprécie',
+	'likes:delete:confirm' => 'Etes-vous sûr que vous voulez retirer cette appréciation ?',
 
-	'river:likes' => 'aiment %s %s',
+	'river:likes' => 'apprécient %s %s',
 
 	// notifications. yikes.
-	'likes:notifications:subject' => '%s aime votre message "%s"',
-	'likes:notifications:body' =>
-'Bonjour %1$s,
+	'likes:notifications:subject' => '%s apprécie votre message "%s"',
+	'likes:notifications:body' => 'Bonjour %1$s,
 
-%2$s aime votre message \'%3$s\' sur %4$s
+%2$s apprécie votre message \'%3$s\' sur %4$s
 
-Voir votre message originale ici :
+Voir votre message original ici :
 
 %5$s
 
