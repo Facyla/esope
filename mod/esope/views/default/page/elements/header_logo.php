64c9119a953ef727b7198bcd3e423937d7767375
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-10-16 01:53:07 +0200

    Esope topbar

--- mod/esope/views/default/page/elements/header_logo.php
+++ mod/esope/views/default/page/elements/header_logo.php
@@ -7,13 +7,14 @@ $site = elgg_get_site_entity();
 $site_name = $site->name;
 $site_url = elgg_get_site_url();
 
-// Entête configurable
+// Optional override : entête configurable
 $header = elgg_get_plugin_setting('header', 'esope');
 if (!empty($header)) {
 	echo $header;
 	return;
 }
 ?>
+
 <h1>
 	<a class="elgg-heading-site" href="<?php echo $site_url; ?>">
 		<?php echo $site_name; ?>
