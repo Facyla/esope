84e80eea021ea8647fa3b04b7f7d81e39eef9a22
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-10-20 11:18:54 +0200

    AU subgroups function update

--- mod/esope/start.php
+++ mod/esope/start.php
@@ -177,7 +177,7 @@ function esope_init() {
 	// Modification de la page de listing des sous-groupes
 	if (elgg_is_active_plugin('au_subgroups')) {
 		// route some urls that go through 'groups' handler
-		elgg_unregister_plugin_hook_handler('route', 'groups', 'au_subgroups_groups_router');
+		elgg_unregister_plugin_hook_handler('route', 'groups', 'AU\SubGroups\groups_router');
 		elgg_register_plugin_hook_handler('route', 'groups', 'esope_subgroups_groups_router', 499);
 		
 		/* au_subgroups prevents users from being invited to subgroups they can't join
@@ -186,7 +186,7 @@ function esope_init() {
 		 * AND handle the au_subgroup case by filtering the passed GUID to in the invite action
 		 *    which avoids breaking the whole process for some users (especially if we register them directly into the group)
 		 */
-		elgg_unregister_plugin_hook_handler('action', 'groups/invite', 'au_subgroups_group_invite');
+		elgg_unregister_plugin_hook_handler('action', 'groups/invite', 'AU\SubGroups\group_invite');
 		elgg_register_plugin_hook_handler('action', 'groups/invite', 'esope_au_subgroups_group_invite');
 	}
 	
@@ -1783,7 +1783,7 @@ function esope_login_user_action($event, $type, $user) {
 								// Handle subgroups cases
 								if (elgg_is_active_plugin('au_subgroups')) {
 									system_message(elgg_echo('esope:subgroups:tryjoiningparent', array($group->name)));
-									while ($parent = au_subgroups_get_parent_group($group)) {
+									while ($parent = AU\SubGroups\get_parent_group($group)) {
 										//  Join only if parent group is public membership or if we have a join pending
 										if (!$parent->isMember($user) && ($parent->isPublicMembership() || in_array($parent->guid, $user->join_groups))) {
 											// Join group, or add to join list
