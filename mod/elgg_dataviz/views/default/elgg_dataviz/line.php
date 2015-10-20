2ff96d3b584c57fb5907c2a2d8b12a8cbae575e2
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-10-20 23:42:07 +0200

    Dataviz updates

--- mod/elgg_dataviz/views/default/elgg_dataviz/line.php
+++ mod/elgg_dataviz/views/default/elgg_dataviz/line.php
@@ -4,12 +4,12 @@
 // Available libraries
 $libraries = array('chartjs');
 // Default visualisation library
-$default_lib = 'chartjs'
+$default_lib = 'chartjs';
 
 // Choose custom visualisation library
 $lib = elgg_extract('lib', $vars, $default_lib);
 if (!in_array($lib, $libraries)) { $lib = $default_lib; }
 unset($vars['lib']);
 
-echo elgg_view("$lib/line", $vars);
+echo elgg_view("elgg_dataviz/$lib/line", $vars);
 
