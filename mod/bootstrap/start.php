fb390fb55bbb486a9187760cdea18ee42d3c63d5
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-10-21 14:02:22 +0200

    Update bootstrap to latest

--- mod/bootstrap/start.php
+++ mod/bootstrap/start.php
@@ -20,11 +20,15 @@ function bootstrap_init() {
 	*/
 	
 	// Register JS scripts and CSS
-	$vendors_url = '/mod/bootstrap/vendors/bootstrap-3.3.5/';
+	// Note : using 4.0 alpha because there is a really bad CSS declaration in previous versions 
+	// ie: .hidden { display:none !important; }
+	// which breaks most jquery and Elgg toggles
+	//$vendors_url = '/mod/bootstrap/vendors/bootstrap-3.3.5/';
+	$vendors_url = '/mod/bootstrap/vendors/bootstrap-4.0.0/';
 	// Main style
 	elgg_register_css('bootstrap', $vendors_url . 'css/bootstrap.min.css');
-	// Optional theme
-	//elgg_register_css('bootstrap-theme', $vendors_url . 'css/bootstrap.theme.min.css');
+	// Optional theme (not available on 4.0.0)
+	//elgg_register_css('bootstrap-theme', $vendors_url . 'css/bootstrap-theme.min.css');
 	
 	// JS scripts
 	// Bootstrap core
@@ -32,10 +36,11 @@ function bootstrap_init() {
 		'src' => $vendors_url . 'js/bootstrap.min.js',
 	));
 	
-	// Load JS and CSS
-	elgg_load_css('bootstrap');
+	// Load JS and CSS : leave this up to the theme
+	// so we can activate the plugin without applying its styles on every page
+	//elgg_load_css('bootstrap');
 	//elgg_load_css('bootstrap-theme');
-	elgg_require_js("bootstrap");
+	//elgg_require_js("bootstrap");
 	
 }
 
