b6556e921ef746bd8231139a44ca069a6c86c024
Facyla ~ Florian DANIEL <facyla@gmail.com>
2015-11-10 16:26:38 +0100

    Better translation icons

--- mod/multilingual/views/default/multilingual/css.php
+++ mod/multilingual/views/default/multilingual/css.php
@@ -2,7 +2,8 @@
 
 .elgg-menu-multilingual img { border: 1px solid transparent; }
 
-.elgg-menu-multilingual.elgg-selected img { border: 1px solid black; box-shadow: 0px 0px 2px 1px rgba(255,00,00,1); }
+.multilingual-main img { border: 1px solid black; box-shadow: 0 0 2px 1px #999; }
 
-.elgg-menu-multilingual.multilingual-main img { box-shadow: 0 0 2px 1px #999; }
+.elgg-menu-multilingual.elgg-selected img { box-shadow: 0px 0px 2px 1px rgba(255,00,00,1); }
+
 
