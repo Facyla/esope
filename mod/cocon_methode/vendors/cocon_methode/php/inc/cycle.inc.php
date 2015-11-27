b9b74114639f67e2d232abe62fad485976524d15
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2015-11-27 18:17:15 +0100

    Debug Kit

--- mod/cocon_methode/vendors/cocon_methode/php/inc/cycle.inc.php
+++ mod/cocon_methode/vendors/cocon_methode/php/inc/cycle.inc.php
@@ -19,7 +19,7 @@
 			return false;
 		}
 		
-		if (is_bool($result)) error_log("Cocon Kit : $result / $gid / $sql / ".print_r($result, true)); // debug
+		if (is_bool($result)) error_log("Cocon Kit : $result / $gid / $sql / ".mysql_num_rows($result)); // debug
 		if(mysql_num_rows($result) == 0){
 			return false;
 		}
