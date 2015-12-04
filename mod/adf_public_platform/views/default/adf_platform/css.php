d6ed66f1d8b6b5d79a73e64776139b0b653f927a
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2015-12-04 16:41:21 +0100

    CSS for sub and sup

--- mod/adf_public_platform/views/default/adf_platform/css.php
+++ mod/adf_public_platform/views/default/adf_platform/css.php
@@ -100,6 +100,22 @@ html {
 
 html { -webkit-font-smoothing: antialiased; }
 
+/* Sub and sup CSS style - from https://gist.github.com/unruthless/413930 */
+sub, sup {
+  /* Specified in % so that the sup/sup is the right size relative to the surrounding text */
+  font-size: 75%;
+  /* Zero out the line-height so that it doesn't interfere with the positioning that follows */
+  line-height: 0;
+  /* Where the magic happens: makes all browsers position the sup/sup properly, relative to the surrounding text */
+  position: relative;
+  /* Note that if you're using Eric Meyer's reset.css, this is already set and you can remove this rule */
+  vertical-align: baseline;
+}
+/* Move the superscripted text up */
+sup { top: -0.5em; }
+/* Move the subscripted text down, but only half as far down as the superscript moved up */
+sub { bottom: -0.25em; }
+
 /* ELEMENTS ET CLASSES DE BASE - BASIC CLASSES AND ELEMENTS */
 pre, code { word-break:break-all; }
 .mts { margin-right:10px; }
