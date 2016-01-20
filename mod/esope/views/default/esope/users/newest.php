4993e4143c3ff6ccf16906769a545a437cd836da
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-01-20 09:03:45 +0100

    Add param

--- mod/esope/views/default/esope/users/newest.php
+++ mod/esope/views/default/esope/users/newest.php
@@ -1,8 +1,10 @@
-<?php $newest_members = elgg_get_entities(array('types' => 'user', 'limit' => 24));
+<?php
+$limit = elgg_extract('limit', $vars, 24);
+$newest_members = elgg_get_entities(array('types' => 'user', 'limit' => $limit));
 ?>
 
 <div class="sidebarBox">
-	<h3><a href="<?php echo elgg_get_site_url(); ?>members/newest"><?php echo elgg_echo('esope:members:newest') ?></a></h3>
+	<h3><a href="<?php echo elgg_get_site_url(); ?>members/newest"><?php echo elgg_echo('esope:members:newest'); ?></a></h3>
 	<?php echo elgg_view('esope/users/members', array('members' => $newest_members)); ?>
 </div>
 
