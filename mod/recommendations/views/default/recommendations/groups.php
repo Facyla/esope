2c98e2492c40e6011306d80a8bc1912ffb01a1e8
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-01-22 20:34:50 +0100

    Recommendtions views

---
+++ mod/recommendations/views/default/recommendations/groups.php
@@ -0,0 +1,41 @@
+<?php
+
+$user = $vars['entity'];
+$limit = $vars['limit'];
+$list_style = $vars['list_style'];
+
+// GROUPES 
+$featured_groups = elgg_get_entities_from_metadata(array('type' => 'group', 'limit' => 0, 'metadata_name_value_pairs' => array('name' => 'featured_group', 'value' => 'yes')));
+foreach($featured_groups as $ent) {
+	if (!$ent->isMember($user)) {
+		/*
+		$icon = '<a href="' . $ent->getURL() . '"><img src="' . $ent->getIcon('small') . '" /></a>';
+		$info = '<p><strong>' . $ent->name . '</strong>';
+		$info .= '<p>' . $ent->getMembers(null, null, true) . ' membres</p>';
+		$content .= elgg_view_listing($icon, $info);
+		*/
+		$content .= '<a href="' . $ent->getURL() . '"><img src="' . $ent->getIcon('small') . '" /></a>';
+	}
+}
+$content .= '<div class="clearfloat"></div>';
+
+$name_metastring_id = elgg_get_metastring_id('featured_group');
+$value_metastring_id = elgg_get_metastring_id('yes');
+$dbprefix = elgg_get_config('dbprefix');
+$exclude_featured = "NOT EXISTS (SELECT 1 FROM {$dbprefix}metadata md WHERE md.entity_guid = e.guid AND md.name_id = $name_metastring_id AND md.value_id = $value_metastring_id)";
+$popular_groups = elgg_get_entities_from_relationship_count(array('type' => 'group', 'relationship' => 'member', 'inverse_relationship' => false, 'wheres' => array($exclude_featured), 'limit' => 6));
+foreach($popular_groups as $ent) {
+	if (!$ent->isMember($user)) {
+		/*
+		$icon = '<a href="' . $ent->getURL() . '"><img src="' . $ent->getIcon('small') . '" /></a>';
+		$info = '<p><strong>' . $ent->name . '</strong>';
+		$info .= '<p>' . $ent->getMembers(null, null, true) . ' membres</p>';
+		$content .= elgg_view_listing($icon, $info);
+		*/
+		$content .= '<a href="' . $ent->getURL() . '"><img src="' . $ent->getIcon('small') . '" /></a>';
+	}
+}
+$content .= '<div class="clearfloat"></div>';
+
+echo $content;
+
---
+++ mod/recommendations/views/default/recommendations/users.php
@@ -0,0 +1,30 @@
+<?php
+
+$user = $vars['entity'];
+$limit = $vars['limit'];
+$list_style = $vars['list_style'];
+
+$recommendations = recommendations_get_users($user, 30, $limit);
+
+
+$content .= "<h3>Recommandations de contacts pour {$user->name}</h3>";
+if (count($recommendations > 0)) {
+	foreach ($recommendations as $person) {
+		//$mutuals = count($person['mutuals']);
+		//$shared_groups = count($person['groups']);
+		//$icon = '<a href="' . $ent->getURL() . '"><img src="' . $ent->getIcon('large') . '" /></a>';
+		/*
+		$info = '<p><strong>' . $person['entity']->name . '</strong>';
+		if ($mutuals > 0) $info = ' &nbsp; ' . $mutuals . ' contacts comuns';
+		if ($shared_groups > 0) $info .= ' &nbsp; ' . $shared_groups . ' groupes partagés';
+		$info .= '</p>';
+		*/
+		$content .= elgg_view('profile/icon', array('entity' => $person['entity'], 'size' => 'small'));
+	}
+} else {
+	//$content .= '<p>' . "Pas de recommandation de contact pour le moment.</p><p>Commencez par rejoindre des groupes et entrer en contact avec quelques personnes pour avoir des recommandations personnalisées" . '</p>';
+}
+$content .= '<div class="clearfloat"></div>';
+
+echo $content;
+
