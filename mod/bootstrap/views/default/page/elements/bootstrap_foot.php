27226d5a4471567c972c52e16e63e77bb06a0dce
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-02-10 11:51:40 +0100

    New Bootstrap version + views (work in progress)

---
+++ mod/bootstrap/views/default/page/elements/bootstrap_foot.php
@@ -0,0 +1,17 @@
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="../bower_components/raphael/raphael-min.js"></script>
+    <script src="../bower_components/morrisjs/morris.min.js"></script>
+    <script src="../js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+    
