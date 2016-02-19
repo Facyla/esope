e92fb3a040538e027d3152e4a82d20545f9bd91d
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-02-19 11:58:08 +0100

    Update styles

--- mod/esope/views/default/css/digest/esope.php
+++ mod/esope/views/default/css/digest/esope.php
@@ -1,6 +1,6 @@
 
-.digest-groups .table-item { float:left; width:120px; min-height:170px; text-align: center; padding: 3px 4px 3px 16px; line-height: 1; font-size:12px; }
-.digest-profile .table-item { float:left; width:120px; min-height:170px; text-align: center; padding: 3px 4px 3px 16px; line-height: 1; font-size:12px; }
+.digest-groups .table-item { display:inline-block; width:120px; min-height:170px; text-align: center; padding: 3px 4px 3px 16px; line-height: 1; font-size:12px; }
+.digest-profile .table-item { display:inline-block; width:120px; min-height:170px; text-align: center; padding: 3px 4px 3px 16px; line-height: 1; font-size:12px; }
 .digest-groups img, .digest-groups img { margin-bottom: 3px; }
 
 
