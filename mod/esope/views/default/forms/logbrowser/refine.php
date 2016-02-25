fc07035cc4ee9dbd01192e523cdefbeff2fc57f4
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-02-25 13:29:04 +0100

    Log browser improved filters

--- mod/esope/views/default/forms/logbrowser/refine.php
+++ mod/esope/views/default/forms/logbrowser/refine.php
@@ -43,21 +43,27 @@ $event_opt = esope_get_log_distinct_values('event');
 
 // Form fields
 $form = '';
-	$form .= '<p><label>' . elgg_echo('logbrowser:user') . ' ' . elgg_view('input/text', array('name' => 'search_username', 'value' => $username, 'style' => 'max-width:25ex')) . '</label> &nbsp; ';
-	$form .= '<label>' . elgg_echo('logbrowser:user_guid') . ' ' . elgg_view('input/text', array('name' => 'user_guid', 'value' => $user_guid, 'style' => 'max-width:10ex')) . '</label></p>';
-$form .= '<div class="elgg-col elgg-col-1of2">';
+$form .= '<p>';
+	$form .= '<label>' . elgg_echo('logbrowser:user') . ' ' . elgg_view('input/text', array('name' => 'search_username', 'value' => $username, 'style' => 'max-width:25ex')) . '</label>';
+	$form .= ' &nbsp; ';
+	$form .= '<label>' . elgg_echo('logbrowser:user_guid') . ' ' . elgg_view('input/text', array('name' => 'user_guid', 'value' => $user_guid, 'style' => 'max-width:10ex')) . '</label>';
+$form .= '</p>';
 
-	$form .= '<p><label>' . elgg_echo('logbrowser:ip_address') . ' ' . elgg_view('input/text', array('name' => 'ip_address', 'value' => $ip_address, 'style' => 'max-width:17ex')) . '</label></p>';
-
-	$form .= '<p><label>' . elgg_echo('logbrowser:starttime') . ' ' . elgg_view('input/text', array('name' => 'timelower', 'value' => $lowerval)) . '</label></p>';
-
-	$form .= '<p><label>' . elgg_echo('logbrowser:endtime') . ' ' . elgg_view('input/text', array('name' => 'timeupper', 'value' => $upperval))  . '</label></p>';
-$form .= '</div>';
-$form .= '<div class="elgg-col elgg-col-1of2">';
-	$form .= '<p><label>' . elgg_echo('logbrowser:entity_type') . ' ' . elgg_view('input/dropdown', array('name' => 'entity_type', 'value' => $entity_type, 'options_values' => $entity_type_opt)) . '</label></p>';
-	$form .= '<p><label>' . elgg_echo('logbrowser:entity_subtype') . ' ' . elgg_view('input/dropdown', array('name' => 'entity_subtype', 'value' => $entity_subtype, 'options_values' => $entity_subtype_opt)) . '</label></p>';
-	$form .= '<p><label>' . elgg_echo('logbrowser:object_guid') . ' ' . elgg_view('input/text', array('name' => 'object_guid', 'value' => $object_guid, 'style' => 'max-width:10ex')) . '</label></p>';
-	$form .= '<p><label>' . elgg_echo('logbrowser:event') . ' ' . elgg_view('input/dropdown', array('name' => 'event', 'value' => $event, 'options_values' => $event_opt)) . '</label></p>';
+$form .= '<div class="elgg-grid">';
+	
+	$form .= '<div class="elgg-col elgg-col-1of2">';
+		$form .= '<p><label>' . elgg_echo('logbrowser:ip_address') . ' ' . elgg_view('input/text', array('name' => 'ip_address', 'value' => $ip_address, 'style' => 'max-width:17ex')) . '</label></p>';
+		$form .= '<p><label>' . elgg_echo('logbrowser:starttime') . ' ' . elgg_view('input/text', array('name' => 'timelower', 'value' => $lowerval)) . '</label></p>';
+		$form .= '<p><label>' . elgg_echo('logbrowser:endtime') . ' ' . elgg_view('input/text', array('name' => 'timeupper', 'value' => $upperval))  . '</label></p>';
+	$form .= '</div>';
+	
+	$form .= '<div class="elgg-col elgg-col-1of2">';
+		$form .= '<p><label>' . elgg_echo('logbrowser:entity_type') . ' ' . elgg_view('input/dropdown', array('name' => 'entity_type', 'value' => $entity_type, 'options_values' => $entity_type_opt)) . '</label></p>';
+		$form .= '<p><label>' . elgg_echo('logbrowser:entity_subtype') . ' ' . elgg_view('input/dropdown', array('name' => 'entity_subtype', 'value' => $entity_subtype, 'options_values' => $entity_subtype_opt)) . '</label></p>';
+		$form .= '<p><label>' . elgg_echo('logbrowser:object_guid') . ' ' . elgg_view('input/text', array('name' => 'object_guid', 'value' => $object_guid, 'style' => 'max-width:10ex')) . '</label></p>';
+		$form .= '<p><label>' . elgg_echo('logbrowser:event') . ' ' . elgg_view('input/dropdown', array('name' => 'event', 'value' => $event, 'options_values' => $event_opt)) . '</label></p>';
+	$form .= '</div>';
+	
 $form .= '</div>';
 
 $form .= '<div class="clearfloat"></div>';
