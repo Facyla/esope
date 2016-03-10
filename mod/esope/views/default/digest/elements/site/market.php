8a71897683458f9bb54aaed72f86b84478717737
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-03-10 19:12:27 +0100

    Digest elements updates

--- mod/esope/views/default/digest/elements/site/market.php
+++ mod/esope/views/default/digest/elements/site/market.php
@@ -10,10 +10,10 @@
 // only show markets that are published
 $dbprefix = elgg_get_config("dbprefix");
 
-//$market_status_name_id = elgg_get_metastring_id("status");
-//$market_published_value_id = elgg_get_metastring_id("published");
+//$offer_status_name_id = elgg_get_metastring_id("status");
+//$offer_published_value_id = elgg_get_metastring_id("published");
 
-$market_options = array(
+$offer_options = array(
 	"type" => "object",
 	"subtype" => "market",
 	"limit" => 3,
@@ -26,65 +26,79 @@ $market_options = array(
 		"JOIN " . $dbprefix . "metadata bm ON e.guid = bm.entity_guid"
 	),
 	"wheres" => array(
-		"bm.name_id = " . $market_status_name_id,
-		"bm.value_id = " . $market_published_value_id
+		"bm.name_id = " . $offer_status_name_id,
+		"bm.value_id = " . $offer_published_value_id
 	)
 	*/
 );
 
-$markets = elgg_get_entities($market_options);
-if (!empty($markets)) {
+$offers = elgg_get_entities($offer_options);
+if (!empty($offers)) {
 	$title = elgg_view("output/url", array(
 		"text" => elgg_echo("market:title"),
 		"href" => "market/all",
 		"is_trusted" => true
 	));
 	
-	$latest_markets = "";
+	$content = '';
 	
-	foreach ($markets as $market) {
-		$market_url = $market->getURL();
-		$owner = $market->getOwnerEntity();
-		$container = $market->getContainerEntity();
-		$category = "<b>" . elgg_echo('market:category') . ":</b> " . elgg_echo("market:category:{$market->marketcategory}");
-		$type = "<b>" . elgg_echo('market:type') . ":</b> " . elgg_echo("market:type:{$market->market_type}");
-		$excerpt = elgg_get_excerpt($market->description);
+	foreach ($offers as $offer) {
+		$offer_content = "";
+		$offer_url = $offer->getURL();
+		$owner = $offer->getOwnerEntity();
+		$container = $offer->getContainerEntity();
+		$category = "<b>" . elgg_echo('market:category') . ":</b> " . elgg_echo("market:category:{$offer->marketcategory}");
+		$type = "<b>" . elgg_echo('market:type') . ":</b> " . elgg_echo("market:type:{$offer->market_type}");
+		$excerpt = elgg_get_excerpt($offer->description);
 		
 		
-		$latest_markets .= "<div class='digest-market'>";
+		$offer_content .= "<div class='digest-market'>";
 		/*
-		if ($market->icontime) {
+		if ($offer->icontime) {
 			$icon = elgg_view("output/img", array(
-					"src" => $market->getIconURL("medium")
+					"src" => $offer->getIconURL("medium")
 				));
 		}
 		*/
 		$icon = elgg_view('output/img', array(
-				'src' => "market/image/{$market->guid}/1/medium/{$market->time_updated}",
+				'src' => "market/image/{$offer->guid}/1/medium/{$offer->time_updated}",
 				'class' => 'market-image-list',
-				'alt' => $market->guid,
+				'alt' => $offer->guid,
+			));
+		$offer_img = elgg_view('output/url', array(
+			'text' => $icon,
+			'href' => market_url,
+			'is_trusted' => true,
 			));
 		
-		$latest_markets .= elgg_view("output/url", array(
-			"text" => $icon,
-			"href" => $market_url,
+		$offer_content .= "<span>";
+		$offer_content .= "<h4>";
+		$offer_content .= elgg_view("output/url", array(
+			"text" => $offer->title,
+			"href" => $offer_url,
 			"is_trusted" => true
 		));
+		$offer_content .= "</h4>";
+		$offer_content .= $category;
+		$offer_content .= $type;
+		$offer_content .= $excerpt;
+		$offer_content .= "</span>";
+		$offer_content .= "</div>";
 		
-		$latest_markets .= "<span>";
-		$latest_markets .= "<h4>";
-		$latest_markets .= elgg_view("output/url", array(
-			"text" => $market->title,
-			"href" => $market_url,
-			"is_trusted" => true
-		));
-		$latest_markets .= "</h4>";
-		$latest_markets .= $category;
-		$latest_markets .= $type;
-		$latest_markets .= $excerpt;
-		$latest_markets .= "</span>";
-		$latest_markets .= "</div>";
+		/*
+		$params = array(
+			'entity' => $post,
+			'metadata' => $metadata,
+			'subtitle' => $subtitle,
+			'tags' => $tags,
+			'content' => $excerpt,
+		);
+		$params = $params + $vars;
+		$offer_content = elgg_view('object/elements/summary', $params);
+		*/
+		
+		$content .= elgg_view_image_block($offer_img, $offer_content, array('class' => 'market-list-block'));
 	}
 	
-	echo elgg_view_module("digest", $title, $latest_markets);
+	echo elgg_view_module("digest", $title, $content);
 }
