8f6882812f5bc174dc4845ebde885743161d520b
Facyla ~ Florian DANIEL <facyla@gmail.com>
2016-03-18 15:55:51 +0100

    New plugin  DB Backup from Pawel Sroka

---
+++ mod/db_backup/actions/backup/db/load.php
@@ -0,0 +1,15 @@
+<?php
+$fileName = get_input('backup_file');
+
+try {
+	if (db_backup::doRestore($fileName)) {
+		system_message(elgg_echo('db_backup:action:restore:ok', array($fileName)));
+	} else {
+		register_error(elgg_echo('db_backup:action:restore:fail', array(
+			db_backup::getErrorCode(),
+			db_backup::getErrorMessage()
+		)));
+	}
+} catch (RuntimeException $e) {
+	register_error($e->getMessage());
+}
---
+++ mod/db_backup/actions/backup/db/save.php
@@ -0,0 +1,13 @@
+<?php
+try {
+	if (db_backup::doBackup()) {
+		system_message(elgg_echo('db_backup:action:backup:ok', array(db_backup::getLastBackupFileName())));
+	} else {
+		register_error(elgg_echo('db_backup:action:backup:fail', array(
+			db_backup::getErrorCode(),
+			db_backup::getErrorMessage()
+		)));
+	}
+} catch (RuntimeException $e) {
+	register_error($e->getMessage());
+}
