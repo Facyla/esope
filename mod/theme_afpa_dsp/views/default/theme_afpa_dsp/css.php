8803fbbe97ed8f203fbb649ae2cecac742aea47d
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-05-18 18:57:06 +0200

    Short listing

--- mod/theme_afpa_dsp/views/default/theme_afpa_dsp/css.php
+++ mod/theme_afpa_dsp/views/default/theme_afpa_dsp/css.php
@@ -4,7 +4,7 @@
 #transverse .elgg-menu-navigation li a { padding: 0.5em 0.6em; }
 
 
-.kdb-featured { border:0; }
+.kdb-featured { border-right:0; }
 .kdb-featured:hover { background: #F0FFF0; }
 
 
