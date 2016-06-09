31da290f5893225077c272174d32c840727404c3
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-06-09 20:26:52 +0200

    Search user by email

--- mod/esope/pages/esope/tools/users_email_search.php
+++ mod/esope/pages/esope/tools/users_email_search.php
@@ -17,15 +17,33 @@ $content .= '<form><p><label>Email à rechercher' . elgg_view('input/text', arra
 $content .= '<div class="clearfloat"></div>';
 if (!empty($email)) {
 	$content .= '<h3>Comptes correspondant à "' . $email . '"</h3>';
+	// Exact search
+	$content .= "<h4>Recherche exacte</h4>";
 	$users = get_user_by_email($email);
 	if ($users) {
 		foreach ($users as $ent) {
 			$icon = elgg_view_entity_icon($ent, 'tiny');
-			$text = $ent->name . ' : <a href="' . $ent->getURL() . '">' . $ent->username . ' (GUID ' . $ent->guid . ')</a>';
+			$text = $ent->name . ' : <a href="' . $ent->getURL() . '">' . $ent->username . ' (GUID ' . $ent->guid . ') => ' . $ent->email . '</a>';
 			$content .= elgg_view_image_block($icon, $text);
 		}
 	} else {
-		$content .= "<p>Aucun résultat</p>";
+		$content .= "<p>Aucun résultat avec l'email : $email</p>";
+	}
+	// Match search search
+	$content .= "<h4>Recherche par correspondance</h4>";
+	$users = elgg_get_entities(array(
+			'type' => 'user',
+			'joins' => array("INNER JOIN " . elgg_get_config("dbprefix") . "users_entity ue USING(guid)"),
+			'wheres' => array("ue.email LIKE '%$email%'"),
+		));
+	if ($users) {
+		foreach ($users as $ent) {
+			$icon = elgg_view_entity_icon($ent, 'tiny');
+			$text = $ent->name . ' : <a href="' . $ent->getURL() . '">' . $ent->username . ' (GUID ' . $ent->guid . ') => ' . $ent->email . '</a>';
+			$content .= elgg_view_image_block($icon, $text);
+		}
+	} else {
+		$content .= "<p>Aucun email contenant : $email</p>";
 	}
 }
 
