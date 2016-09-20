f7264e7720fb10a9814d0f205321c7611e9155f5
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-09-20 19:38:48 +0200

    Modif inscription

--- mod/theme_transitions2/views/default/theme_transitions2/fing_register.php
+++ mod/theme_transitions2/views/default/theme_transitions2/fing_register.php
@@ -2,9 +2,9 @@
 // 3 méthodes d'inscription : Fing, pré-remplissage via RS, et inscription
 
 echo '<p class="transitions2-register-subtitle">' . elgg_echo('theme_transitions:register:subtitle') . '</p>';
-echo '<hr />';
 echo '<h3>' . elgg_echo('theme_transitions:registerwithfing:title') . '</h3>';
 echo '<p>' . elgg_echo('theme_transitions:registerwithfing') . '</p>';
+echo '<br />';
 
 if (elgg_is_active_plugin('hybridauth')) {
 	echo '<hr />';
