e049c55136a57442a4abc9dbc17d150ad90f35d4
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2017-07-13 18:20:43 +0200

    Wiki rendering

--- mod/esope/views/default/pages/sub-pages.php
+++ mod/esope/views/default/pages/sub-pages.php
@@ -13,6 +13,7 @@ $subpages = esope_list_subpages($page, 'url', false);
 
 if ($subpages) {
 	echo '<div class="esope-subpages-menu elgg-output">';
+	echo '<a href="' . elgg_get_site_url() . 'pages/add/' . $page->guid . '" class="elgg-button elgg-button-action float-alt">' . elgg_echo('pages:newchild') . '</a>';
 	echo '<h3>' . elgg_echo('esope:pages:subpages') . '</h3>';
 	echo $subpages;
 	echo '</div>';
