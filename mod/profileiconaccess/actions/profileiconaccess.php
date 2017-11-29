53cca1de533e92d36eb106524c7153a2afab862b
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2017-11-29 10:42:41 +0100

    New version with admin edit support + debug

--- mod/profileiconaccess/actions/profileiconaccess.php
+++ mod/profileiconaccess/actions/profileiconaccess.php
@@ -1,23 +1,22 @@
 <?php
-
 namespace AU\ProfileIconAccess;
 
 $guid = (int) get_input('guid');
 $access = (int) get_input('access');
 $user = get_user($guid);
 
-if (!user || !$user->canEdit()) {
-  register_error(elgg_echo('profileiconaccess:error'));
-  forward();
+if (!$user || !$user->canEdit()) {
+	register_error(elgg_echo('profileiconaccess:error'));
+	forward();
 }
 
-$id = create_metadata($user->guid, 'iconaccess', $access, 'integer', $user->guid, $access);
+// Metadata should be public, so any logged in user can access it and determiner that icon should not be displayed
+$id = create_metadata($user->guid, 'iconaccess', $access, 'integer', $user->guid, 2);
 
 if ($id) {
-  system_message(elgg_echo('profileiconaccess:success'));
-}
-else {
-  register_error(elgg_echo('profileiconaccess:error'));
+	system_message(elgg_echo('profileiconaccess:success'));
+} else {
+	register_error(elgg_echo('profileiconaccess:error'));
 }
 
 forward(REFERER);
--- mod/profileiconaccess/views/default/forms/profileiconaccess.php
+++ mod/profileiconaccess/views/default/forms/profileiconaccess.php
@@ -4,19 +4,20 @@ namespace AU\ProfileIconAccess;
 
 elgg_require_js('profileiconaccess');
 
-$user = elgg_get_logged_in_user_entity();
+//$user = elgg_get_logged_in_user_entity();
+$user = elgg_get_page_owner_entity();
 
 $access = ($user->iconaccess === NULL) ? ACCESS_PUBLIC : $user->iconaccess;
 
 
 echo '<div class="profileiconaccess">';
-echo '<label for="profileiconaccess-select">' . elgg_echo('profileiconaccess:label') . ' </label>';
-echo elgg_view('input/access', array(
-			'name' => 'profileaccessicon',
-			'value' => $access,
-			'id' => 'profileiconaccess-select',
-		));
-echo '<div class="profileiconaccess-throbber">';
-echo elgg_view('graphics/ajax_loader');
-echo '</div>';
+	echo '<label for="profileiconaccess-select">' . elgg_echo('profileiconaccess:label') . ' </label>';
+	echo elgg_view('input/access', array(
+				'name' => 'profileaccessicon',
+				'value' => $access,
+				'id' => 'profileiconaccess-select',
+			));
+	echo '<div class="profileiconaccess-throbber">';
+		echo elgg_view('graphics/ajax_loader');
+	echo '</div>';
 echo '</div>';
