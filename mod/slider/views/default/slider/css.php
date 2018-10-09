33ff62972945189fdde22ecf2abe99fc0277d8c4
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2018-10-09 20:17:28 +0200

    Missing CSS selector

--- mod/slider/views/default/slider/css.php
+++ mod/slider/views/default/slider/css.php
@@ -14,7 +14,7 @@
 
 .anythingSlider.anythingSlider-cs-portfolio .anythingControls { position: absolute; bottom: 0; left: 0; right: 0; margin: 0 10% 1rem 10%; padding: 0 !important; }
 .anythingSlider.anythingSlider-cs-portfolio .anythingControls ul.thumbNav { padding-top: 0; }
-.anythingSlider-cs-portfolio .anythingControls ul li { display: inline-block; }
+.anythingSlider.anythingSlider-cs-portfolio .anythingControls ul li { display: inline-block; }
 .anythingSlider.anythingSlider-cs-portfolio .anythingControls ul a { width: 18px; height: 18px; margin: 3px 3px; box-shadow: 0 0 1px 0 rgba(0,0,0,0.8); }
 .anythingSlider.anythingSlider-cs-portfolio .anythingControls a.cur, .anythingSlider.anythingSlider-cs-portfolio .anythingControls a:hover { width: 22px; height: 22px; margin: 0 1px; }
 
