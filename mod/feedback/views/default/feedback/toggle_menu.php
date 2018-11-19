f724e83ecc42df449c5125b558ad5269a129b113
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2018-11-19 14:04:19 +0100

    Plugin replaced by own repo

--- mod/feedback/views/default/feedback/toggle_menu.php
+++
@@ -1,6 +0,0 @@
-
-<div id="feedBackToggler">
-	<a id="feedBackTogglerLink" href="javascript:void(0)" onclick="FeedBack_Toggle();this.blur();">
-		<?php echo elgg_view('feedback/toggle_button'); ?>
-	</a>
-</div>
