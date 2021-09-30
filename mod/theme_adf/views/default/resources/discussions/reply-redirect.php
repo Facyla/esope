d4ad43407882eec9a6b83f5d4907a9849054dbca
Florian DANIEL aka Facyla <i.daniel.florian@gmail.com>
2021-09-30 15:42:47 +0200

    Prise en charge des URLs des discussions de ELgg 1.12

--- mod/theme_adf/views/default/resources/discussions/reply-redirect.php
+++ mod/theme_adf/views/default/resources/discussions/reply-redirect.php
@@ -4,7 +4,7 @@ $container_guid = get_input('container_guid');
 
 $new_url = elgg_get_site_url() . "comment/view/$guid/$container_guid";
 
-register_error(elgg_echo('theme_adf:discussion-reply:redirect'), [$new_menu]);
+register_error(elgg_echo('theme_adf:discussion-reply:redirect'), [$new_url]);
 
 forward($new_url);
 
