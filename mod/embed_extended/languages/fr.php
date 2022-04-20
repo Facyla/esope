41440f2bfff04c48107d070cc2f398a3f15f4fce
Florian DANIEL aka Facyla <i.daniel.florian@gmail.com>
2022-04-20 17:16:40 +0200

    Traduction de embed_extended

---
+++ mod/embed_extended/languages/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'embed_extended:menu:embed:internal_content' => "Rechercher un contenu",
+	'embed_extended:internal_content:placeholder' => "Saisissez le titre ou le nom à rechercher",
+	'embed_extended:internal_content:match_owner' => "Seulement mes propres contenus",
+	'embed_extended:internal_content:container' => "Seulement le contenu de ce groupe",
+	
+);
+
